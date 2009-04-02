45de21d5442a0e2c651aa932ccbb0aa463d64754
Mark Evans <mark@new-bamboo.co.uk>
2009-04-02 18:24:25 +0100

    Added configurable_attr using some cool metaprogramming!

--- lib/imagetastic/configurable.rb
+++ lib/imagetastic/configurable.rb
@@ -1,4 +1,16 @@
 module Imagetastic
   module Configurable
+    
+    private
+    
+    def configurable_attr attribute, default=nil
+      instance_variable_set "@#{attribute}", default
+      class << self
+        self # Get the singleton class of the extended object
+      end.class_eval do
+        attr_reader attribute # This defines the reader on the extended object
+      end
+    end
+    
   end
 end
--- spec/imagetastic/configurable_spec.rb
+++ spec/imagetastic/configurable_spec.rb
@@ -1,5 +1,29 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
-describe Imagetastic::Configurable do
+class Car
+  extend Imagetastic::Configurable
+
+  configurable_attr :colour
+  configurable_attr :top_speed, 216
+
+end
+
+describe Car do
+
+  it "should provide attr_readers for configurable attributes" do
+    Car.should respond_to(:colour)
+  end
+  
+  it "should not provide attr_writers for configurable attributes" do
+    Car.should_not respond_to(:colour=)
+  end
+  
+  it "should set default values for configurable attributes" do
+    Car.top_speed.should == 216
+  end
+  
+  it "should set the default as nil if not specified" do
+    Car.colour.should be_nil
+  end
 
 end
