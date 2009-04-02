7ba6a273ccee1dd8e505cad46db010568c701ff7
Mark Evans <mark@new-bamboo.co.uk>
2009-04-03 00:46:23 +0100

    Finished 'configurable' module!

--- lib/imagetastic/configurable.rb
+++ lib/imagetastic/configurable.rb
@@ -1,15 +1,37 @@
 module Imagetastic
   module Configurable
     
+    # Exceptions
+    class NothingToConfigure < StandardError; end
+    
+    def configure(&blk)
+      raise NothingToConfigure, "You called configure but there are no configurable attributes" if configuration_hash.empty?
+      struct_class = Struct.new(*configuration_hash.keys)
+      struct = struct_class.new(*configuration_hash.values)
+      yield(struct)
+      struct.each_pair{|k,v| configuration_hash[k] = v }
+    end
+    
+    def configuration
+      configuration_hash.dup
+    end
+    
     private
     
     def configurable_attr attribute, default=nil
-      instance_variable_set "@#{attribute}", default
+      configuration_hash[attribute] = default
       class << self
         self # Get the singleton class of the extended object
       end.class_eval do
-        attr_reader attribute # This defines the reader on the extended object
+        # Define the reader on the extended object
+        define_method(attribute) do
+          configuration_hash[attribute]
+        end
       end
+    end
+    
+    def configuration_hash
+      @configuration_hash ||= {}
     end
     
   end
--- spec/imagetastic/configurable_spec.rb
+++ spec/imagetastic/configurable_spec.rb
@@ -1,29 +1,67 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
-class Car
-  extend Imagetastic::Configurable
+describe Imagetastic::Configurable do
 
-  configurable_attr :colour
-  configurable_attr :top_speed, 216
+  before(:each) do
+    class Car
+      extend Imagetastic::Configurable
+      configurable_attr :colour
+      configurable_attr :top_speed, 216
+      def self.other_method; end
+      def self.other_method=; end
+    end    
+  end
 
-end
+  describe "setup" do
+    it "should provide attr_readers for configurable attributes" do
+      Car.should respond_to(:colour)
+    end
 
-describe Car do
+    it "should not provide attr_writers for configurable attributes" do
+      Car.should_not respond_to(:colour=)
+    end
 
-  it "should provide attr_readers for configurable attributes" do
-    Car.should respond_to(:colour)
+    it "should set default values for configurable attributes" do
+      Car.top_speed.should == 216
+    end
+
+    it "should set the default as nil if not specified" do
+      Car.colour.should be_nil
+    end
+  end
+
+  
+  describe "configuring" do
+    it "should raise an error if there are no configurable attributes" do
+      class None; extend Imagetastic::Configurable; end
+      lambda {
+        None.configure{|c|}
+      }.should raise_error(Imagetastic::Configurable::NothingToConfigure)
+    end
+    
+    it "should allow you to change values" do
+      Car.configure do |c|
+        c.colour = 'red'
+      end
+      Car.colour.should == 'red'
+    end
+    
+    it "should not allow you to call other methods on the object via the configuration" do
+      Car.configure do |c|
+        c.should_not respond_to(:other_method)
+        c.should_not respond_to(:other_method=)
+      end
+    end
   end
   
-  it "should not provide attr_writers for configurable attributes" do
-    Car.should_not respond_to(:colour=)
-  end
-  
-  it "should set default values for configurable attributes" do
-    Car.top_speed.should == 216
-  end
-  
-  it "should set the default as nil if not specified" do
-    Car.colour.should be_nil
+  describe "getting configuration" do
+    it "should return the configuration as a hash" do
+      Car.configuration.should == {:colour => nil, :top_speed => 216}
+    end
+    it "should not allow you to change the configuration via the hash" do
+      Car.configuration[:top_speed] = 555
+      Car.top_speed.should == 216
+    end
   end
 
 end
