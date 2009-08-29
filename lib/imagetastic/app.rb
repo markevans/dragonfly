142a680496d91bfbf4396a06a7e25c7d02dff528
Mark Evans <mark@new-bamboo.co.uk>
2009-08-29 20:11:53 +0100

    Now that url handler works, app spews out the generated params

--- lib/imagetastic/app.rb
+++ lib/imagetastic/app.rb
@@ -2,10 +2,12 @@ module Imagetastic
   class App
     
     def call(env)
-      params = Imagetastic.url_handler.query_to_params(env['QUERY_STRING'])
-      [200, {"Content-Type" => "text/html"}, ["This is imagetastic!"]]
-    rescue UrlHandler::BadParams => e
-      [400, {"Content-Type" => "text/plain"}, [e.message]]
+      params = Imagetastic.url_handler.url_to_params(env['PATH_INFO'], env['QUERY_STRING'])
+    #   image = Imagetastic.datastore.retrieve(image_uid)
+    #   processed_image = Imagetastic.image_processor.process(image, params)
+      [200, {"Content-Type" => params[:encoding][:mime_type]}, [params.inspect]]
+    # rescue UrlHandler::BadParams => e
+    #   [400, {"Content-Type" => "text/plain"}, [e.message]]
     end
 
   end
