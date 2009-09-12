0a3fe9039905e4afd9796d8124a7f901927f2273
Mark Evans <mark@new-bamboo.co.uk>
2009-09-12 14:43:31 +0100

    Got rid of unnecessary 'instance' class and made the app itself configurable. That way, you can have multiple 'apps'

--- config.ru
+++ config.ru
@@ -1,7 +1,9 @@
 require File.dirname(__FILE__) + '/lib/imagetastic'
 
-Imagetastic.url_handler.configure do |c|
+app = Imagetastic::App.new
+
+app.url_handler.configure do |c|
   # c.protect_from_dos_attacks = false
 end
 
-run Imagetastic::App.new
+run app
--- lib/imagetastic/app.rb
+++ lib/imagetastic/app.rb
@@ -1,12 +1,28 @@
+require 'logger'
+
 module Imagetastic
   class App
     
+    include Configurable
+    
+    configurable_attr :datastore do DataStorage::FileDataStore.new end
+    
+    configurable_attr :analyser do Processing::Ragick::Analyser.new end
+    
+    configurable_attr :processor do Processing::RMagick::Processor.new end
+    
+    configurable_attr :encoder do Processing::RMagick::Encoder.new end
+    
+    configurable_attr :url_handler do UrlHandler.new end
+    
+    configurable_attr :log do Logger.new('/var/tmp/imagetastic.log') end
+    
+    
     def call(env)
-      it = Imagetastic::Instance.new
-      parameters = it.url_handler.url_to_parameters(env['PATH_INFO'], env['QUERY_STRING'])
-      temp_object = it.datastore.retrieve(parameters.uid)
-      temp_object = it.processor.process(temp_object, parameters.processing_method, parameters.processing_options)
-      temp_object = it.encoder.encode(temp_object, parameters.mime_type, parameters.encoding)
+      parameters = url_handler.url_to_parameters(env['PATH_INFO'], env['QUERY_STRING'])
+      temp_object = datastore.retrieve(parameters.uid)
+      temp_object = processor.process(temp_object, parameters.processing_method, parameters.processing_options)
+      temp_object = encoder.encode(temp_object, parameters.mime_type, parameters.encoding)
       [200, {"Content-Type" => parameters.mime_type}, temp_object]
     rescue UrlHandler::IncorrectSHA, UrlHandler::SHANotGiven => e
       [400, {"Content-Type" => "text/plain"}, [e.message]]
