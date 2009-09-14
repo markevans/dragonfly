8598da33c7ea51b22fb1654804b3fec491d08c32
Mark Evans <mark@new-bamboo.co.uk>
2009-09-14 10:42:32 +0100

    Removed irrelevant run.rb

--- run.rb
+++
@@ -1,6 +0,0 @@
-require 'rubygems'
-require 'rack'
-require File.dirname(__FILE__) + '/lib/imagetastic'
-
-puts "Running imagetastic..."
-Rack::Handler::Mongrel.run Imagetastic::App.new, :Port => 9292
