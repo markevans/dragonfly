5358a7956f3477786924dff6cfb17f6885f91b40
Mark Evans <mark@new-bamboo.co.uk>
2009-09-21 14:40:21 +0100

    Improved irbrc.rb - lists available uids

--- irbrc.rb
+++ irbrc.rb
@@ -2,19 +2,23 @@ require File.dirname(__FILE__) + '/lib/imagetastic'
 
 include Imagetastic
 
-@app = Imagetastic::App.new
+APP = Imagetastic::App.new
+SAMPLES_DIR = '/Users/markevans/dev/samples/images'
 
-def app
-  @app
-end
+APP.parameters_class.default_mime_type = 'image/jpeg'
 
-app.parameters_class.default_mime_type = 'image/jpeg'
-
-app.parameters_class.add_shortcut(/^\d+x\d+|^\d+x|^x\d+/) do |geometry|
+APP.parameters_class.add_shortcut(/^\d+x\d+|^\d+x|^x\d+/) do |geometry|
   {
     :processing_method => :resize,
     :processing_options => {:geometry => geometry},
   }
 end
 
+available_uids = `find #{APP.datastore.root_path} ! -type d`.split("\n").map do |file|
+  file.sub("#{APP.datastore.root_path}/", '')
+end
+
 puts "Loaded stuff from imagetastic irbrc"
+puts "\nAvailable uids:\n"
+puts available_uids
+puts
