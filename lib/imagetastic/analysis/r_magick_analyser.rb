0436c64aaac4d23a48854a673788755517914e3f
Mark Evans <mark@new-bamboo.co.uk>
2009-10-06 13:59:59 +0100

    Changed analyser not to work on self

--- lib/imagetastic/analysis/r_magick_analyser.rb
+++ lib/imagetastic/analysis/r_magick_analyser.rb
@@ -7,30 +7,30 @@ module Imagetastic
       
       include Utils
       
-      def width
-        image.columns
+      def width(image)
+        rmagick_image(image).columns
       end
       
-      def height
-        image.rows
+      def height(image)
+        rmagick_image(image).rows
       end
       
-      def mime_type
-        mime_type_from_extension(image.format)
+      def mime_type(image)
+        mime_type_from_extension(rmagick_image(image).format)
       end
       
-      def depth
-        image.depth
+      def depth(image)
+        rmagick_image(image).depth
       end
       
-      def number_of_colours
-        image.number_colors
+      def number_of_colours(image)
+        rmagick_image(image).number_colors
       end
       
       private
       
-      def image
-        @image ||= Magick::ImageList.new(self.path).first
+      def rmagick_image(image)
+        Magick::ImageList.new(image.path).first
       end
       
     end
--- spec/imagetastic/analysis/r_magick_analyser_spec.rb
+++ spec/imagetastic/analysis/r_magick_analyser_spec.rb
@@ -5,27 +5,28 @@ describe Imagetastic::Analysis::RMagickAnalyser do
   before(:each) do
     image_path = File.dirname(__FILE__) + '/../../../samples/beach.png'
     @beach = Imagetastic::TempObject.new(File.new(image_path))
-    @beach.extend(Imagetastic::Analysis::RMagickAnalyser)
+    @analyser = Object.new
+    @analyser.extend(Imagetastic::Analysis::RMagickAnalyser)
   end
 
   it "should return the width" do
-    @beach.width.should == 280
+    @analyser.width(@beach).should == 280
   end
   
   it "should return the height" do
-    @beach.height.should == 355
+    @analyser.height(@beach).should == 355
   end
   
   it "should return the mime type" do
-    @beach.mime_type.should == 'image/png'
+    @analyser.mime_type(@beach).should == 'image/png'
   end
   
   it "should return the number of colours" do
-    @beach.number_of_colours.should == 34703
+    @analyser.number_of_colours(@beach).should == 34703
   end
   
   it "should return the depth" do
-    @beach.depth.should == 8
+    @analyser.depth(@beach).should == 8
   end
   
 end
