181551a1c6a44e923208765abf63981c39753163
Mark Evans <mark@new-bamboo.co.uk>
2009-10-07 18:39:34 +0100

    ExtendedTempObject properly delegates to analyser

--- lib/imagetastic/extended_temp_object.rb
+++ lib/imagetastic/extended_temp_object.rb
@@ -8,47 +8,59 @@ module Imagetastic
       attr_accessor :app
     end
     
-    def analyse(analysis_method, *args)
-      check_configured!
-      analyser.send(analysis_method, self, *args)
-    end
-    
     def process(processing_method, *args)
-      check_configured!
       self.class.new(processor.send(processing_method, self, *args))
     end
     
     def process!(processing_method, *args)
-      check_configured!
       modify_self!(processor.send(processing_method, self, *args))
     end
 
     def encode(*args)
-      check_configured!
       self.class.new(encoder.encode(self, *args))
     end
     
     def encode!(*args)
-      check_configured!
       modify_self!(encoder.encode(self, *args))
+    end
+    
+    def respond_to?(method)
+      super || analyser.respond_to?(method)
     end
 
     private
     
+    def method_missing(method, *args, &block)
+      if analyser.respond_to?(method)
+        # Cache the result in an attr_reader
+        result = instance_variable_set("@#{method}", analyser.send(method))
+        self.class.class_eval{ attr_reader method }
+        result
+      else
+        super
+      end
+    end
+    
     def analyser
       self.class.app.analyser
+    rescue NoMethodError
+      raise_not_configured
     end
     
     def processor
       self.class.app.processor
+    rescue NoMethodError
+      raise_not_configured
     end
     
     def encoder
       self.class.app.encoder
+    rescue NoMethodError
+      raise_not_configured
     end
 
-    def check_configured!
-      raise NotConfiguredError, "#{self.class} has no app set" if self.class.app.nil?
+    def raise_not_configured
+      raise NotConfiguredError, "#{self.class} has no app set"
     end
 
   end
---
+++ spec/imagetastic/extended_temp_object_spec.rb
@@ -0,0 +1,56 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+
+describe Imagetastic::ExtendedTempObject do
+  
+  it "should raise an error if not configured with an app" do
+    temp_object = Imagetastic::ExtendedTempObject.new('asdf')
+    lambda{
+      temp_object.process(:dummy)
+    }.should raise_error(Imagetastic::ExtendedTempObject::NotConfiguredError)
+  end
+  
+  describe "when configured correctly" do
+    
+    before(:each) do
+      @analyser = mock('analyser')
+      @processor = mock('processor')
+      @encoder = mock('encoder')
+      @app = mock('app', :analyser => @analyser, :processor => @processor, :encoder => @encoder)
+      @klass = Class.new(Imagetastic::ExtendedTempObject)
+      @klass.app = @app
+      @object = @klass.new('asdf')
+    end
+    
+    describe "analysis" do
+      
+      it "should respond to something that the analyser responds to" do
+        @analyser.should_receive(:respond_to?).with(:some_method).and_return(true)
+        @object.should respond_to(:some_method)
+      end
+      
+      it "should not respond to something that the analyser doesn't respond to" do
+        @analyser.should_receive(:respond_to?).with(:some_method).and_return(false)
+        @object.should_not respond_to(:some_method)
+      end
+
+      it "should delegate the analysis to the analyser" do
+        @analyser.should_receive(:respond_to?).with(:width).and_return(true)
+        @analyser.should_receive(:width).and_return(4)
+        @object.width.should == 4
+      end
+      
+      it "should cache the result so that it doesn't call it a second time" do
+        @analyser.should_receive(:respond_to?).with(:width).and_return(true)
+        @analyser.should_receive(:width).and_return(4)
+        @object.width.should == 4
+
+        @analyser.should_not_receive(:respond_to?).with(:width)
+        @analyser.should_not_receive(:width)
+        @object.width.should == 4
+      end
+      
+    end
+    
+  end
+  
+end
