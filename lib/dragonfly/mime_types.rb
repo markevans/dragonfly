f4767bcb4732fb7a0826b3f8444fd631aec02bfe
Mark Evans <mark@new-bamboo.co.uk>
2009-11-04 18:27:06 +0000

    Added ability to register (and clear) custom mime types

--- lib/dragonfly/mime_types.rb
+++ lib/dragonfly/mime_types.rb
@@ -8,22 +8,41 @@ module Dragonfly
     class << self
 
       def mime_type_for(ext)
-        ext = ext.to_s.sub(/^\./,'')
-        mime_type = MIME::Types.type_for(ext).to_s
-        raise_mime_type_not_found(mime_type) if mime_type.empty?
+        ext = normalize_extension(ext)
+        mime_type = custom_mime_types.index(ext) || MIME::Types.type_for(ext).to_s
+        raise_mime_type_not_found("Couldn't find the mime type for the extension #{ext.inspect}") if mime_type.blank?
         mime_type
       end
     
       def extension_for(mime_type)
-        known_mime_type = MIME::Types[mime_type].first
-        raise_mime_type_not_found(mime_type) if known_mime_type.nil?
-        known_mime_type.extensions.first
+        known_mime_type = custom_mime_types[mime_type] ||
+          MIME::Types[mime_type].first && MIME::Types[mime_type].first.extensions.first
+        raise_mime_type_not_found("Couldn't find the mime type #{mime_type.inspect}") if known_mime_type.blank?
+        known_mime_type
+      end
+      
+      def register(mime_type, file_extension)
+        custom_mime_types[mime_type] = normalize_extension(file_extension)
+      end
+
+      def clear_custom_mime_types!
+        self.custom_mime_types = {}
       end
 
       private
       
-      def raise_mime_type_not_found(mime_type)
-        raise MimeTypeNotFound, "Couldn't find the mime-type #{mime_type.inspect}. You can register mime-types using Dragonfly::MimeTypes.register"
+      def raise_mime_type_not_found(message)
+        raise MimeTypeNotFound, "#{message} - you can register mime-types using Dragonfly::MimeTypes.register"
+      end
+      
+      def custom_mime_types
+        @custom_mime_types ||= {}
+      end
+      
+      attr_writer :custom_mime_types
+      
+      def normalize_extension(ext)
+        ext.to_s.sub(/^\./,'')
       end
       
     end
--- spec/dragonfly/mime_types_spec.rb
+++ spec/dragonfly/mime_types_spec.rb
@@ -2,6 +2,10 @@ require File.dirname(__FILE__) + '/../spec_helper'
 
 describe Dragonfly::MimeTypes do
   
+  after(:each) do
+    Dragonfly::MimeTypes.clear_custom_mime_types!
+  end
+  
   describe "mime_type_for" do
 
     [:png, 'png', '.png'].each do |ext|
@@ -30,6 +34,37 @@ describe Dragonfly::MimeTypes do
       }.should raise_error(Dragonfly::MimeTypes::MimeTypeNotFound)      
     end
     
+  end
+  
+  describe "registering mime-types" do
+    
+    describe "when neither mime type or extension are previously known" do
+      before(:each){ Dragonfly::MimeTypes.register('image/goo', 'goo') }
+      it { Dragonfly::MimeTypes.mime_type_for('goo').should == 'image/goo' }
+      it { Dragonfly::MimeTypes.extension_for('image/goo').should == 'goo' }
+    end
+
+    describe "when file extension isn't previously known" do
+      before(:each){ Dragonfly::MimeTypes.register('image/gif', 'doobie') }
+      it { Dragonfly::MimeTypes.mime_type_for('doobie').should == 'image/gif' }
+      it { Dragonfly::MimeTypes.mime_type_for('gif').should == 'image/gif' }
+      it { Dragonfly::MimeTypes.extension_for('image/gif').should == 'doobie' }
+    end
+
+    describe "when mime type isn't previously known" do
+      before(:each){ Dragonfly::MimeTypes.register('image/dooboo', 'jpg') }
+      it { Dragonfly::MimeTypes.mime_type_for('jpg').should == 'image/dooboo' }
+      it { Dragonfly::MimeTypes.extension_for('image/dooboo').should == 'jpg' }
+    end
+    
+    describe "clearing custom mime-types" do
+      it {
+        Dragonfly::MimeTypes.register('image/gif', 'doobie')
+        Dragonfly::MimeTypes.clear_custom_mime_types!
+        Dragonfly::MimeTypes.extension_for('image/gif').should == 'gif'
+      }
+    end
+
   end
   
 end
