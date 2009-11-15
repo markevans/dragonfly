93b7e4cefcd4a92a404cd7cc5968adeb8cd0d519
Mark Evans <mark@new-bamboo.co.uk>
2009-11-15 19:13:18 +0000

    YARD was having trouble with RMagickConfiguration not being a module, so made it a module

--- lib/dragonfly/app_configuration.rb
+++
@@ -1,9 +0,0 @@
-module Dragonfly
-  class AppConfiguration
-    
-    def apply_configuration(app)
-      raise NotImplementedError
-    end
-    
-  end
-end
--- lib/dragonfly/r_magick_configuration.rb
+++ lib/dragonfly/r_magick_configuration.rb
@@ -1,62 +1,63 @@
 module Dragonfly
-  
-  RMagickConfiguration = AppConfiguration.new
-  
-  def RMagickConfiguration.apply_configuration(app)
-    app.configure do |c|
-      c.analyser do |a|
-        a.register(Analysis::RMagickAnalyser.new)
-      end
-      c.processor do |p|
-        p.register(Processing::RMagickProcessor)
-      end
-      c.encoder = Encoding::RMagickEncoder.new
-      c.parameters do |p|
-        p.default_format = :jpg
-        # Standard resizing like '30x40!', etc.
-        p.add_shortcut(Symbol) do |format|
-          {:format => format}
+  module RMagickConfiguration
+    
+    def self.apply_configuration(app)
+      app.configure do |c|
+        c.analyser do |a|
+          a.register(Analysis::RMagickAnalyser.new)
         end
-        p.add_shortcut(/^\d*x\d*[><%^!]?$|^\d+@$/) do |geometry, match_data|
-          {
-            :processing_method => :resize,
-            :processing_options => {:geometry => geometry}
-          }
+        c.processor do |p|
+          p.register(Processing::RMagickProcessor)
         end
-        # Cropped resizing like '20x50#ne'
-        p.add_shortcut(/^(\d+)x(\d+)#(\w{1,2})?/) do |geometry, match_data|
-          {
-            :processing_method => :resize_and_crop,
-            :processing_options => {:width => match_data[1], :height => match_data[2], :gravity => match_data[3]}
-          }
-        end
-        # Cropping like '30x30+10+10ne'
-        p.add_shortcut(/^(\d+)x(\d+)([+-]\d+)([+-]\d+)(\w{1,2})?/) do |geometry, match_data|
-          {
-            :processing_method => :crop,
-            :processing_options => {
-              :width => match_data[1],
-              :height => match_data[2],
-              :x => match_data[3],
-              :y => match_data[4],
-              :gravity => match_data[5]
+        c.encoder = Encoding::RMagickEncoder.new
+        c.parameters do |p|
+          p.default_format = :jpg
+          # Standard resizing like '30x40!', etc.
+          p.add_shortcut(Symbol) do |format|
+            {:format => format}
+          end
+          p.add_shortcut(/^\d*x\d*[><%^!]?$|^\d+@$/) do |geometry, match_data|
+            {
+              :processing_method => :resize,
+              :processing_options => {:geometry => geometry}
             }
-          }
-        end
-        p.add_shortcut(/^\d*x/, Symbol) do |geometry, format|
-          p.hash_from_shortcut(geometry).merge(:format => format)
-        end
-        p.add_shortcut(:rotate, Numeric) do |_, amount|
-          {
-            :processing_method => :rotate,
-            :processing_options => {:amount => amount, :background_colour => '#0000'}
-          }
-        end
-        p.add_shortcut(:rotate, Numeric, Symbol) do |a, b, format|
-          p.hash_from_shortcut(a,b).merge(:format => format)
+          end
+          # Cropped resizing like '20x50#ne'
+          p.add_shortcut(/^(\d+)x(\d+)#(\w{1,2})?/) do |geometry, match_data|
+            {
+              :processing_method => :resize_and_crop,
+              :processing_options => {:width => match_data[1], :height => match_data[2], :gravity => match_data[3]}
+            }
+          end
+          # Cropping like '30x30+10+10ne'
+          p.add_shortcut(/^(\d+)x(\d+)([+-]\d+)([+-]\d+)(\w{1,2})?/) do |geometry, match_data|
+            {
+              :processing_method => :crop,
+              :processing_options => {
+                :width => match_data[1],
+                :height => match_data[2],
+                :x => match_data[3],
+                :y => match_data[4],
+                :gravity => match_data[5]
+              }
+            }
+          end
+          p.add_shortcut(/^\d*x/, Symbol) do |geometry, format|
+            p.hash_from_shortcut(geometry).merge(:format => format)
+          end
+          p.add_shortcut(:rotate, Numeric) do |_, amount|
+            {
+              :processing_method => :rotate,
+              :processing_options => {:amount => amount, :background_colour => '#0000'}
+            }
+          end
+          p.add_shortcut(:rotate, Numeric, Symbol) do |a, b, format|
+            p.hash_from_shortcut(a,b).merge(:format => format)
+          end
         end
       end
+    
     end
-  end
-  
+    
+  end  
 end
