f6f40297e0feb5638451f80f2270c5574ce844c8
Mark Evans <mark@new-bamboo.co.uk>
2009-11-18 12:34:19 +0000

    Added file command analyser - no specs yet!

---
+++ lib/dragonfly/analysis/file_command_analyser.rb
@@ -0,0 +1,21 @@
+require 'rmagick'
+require 'mime/types'
+
+module Dragonfly
+  module Analysis
+    
+    class FileCommandAnalyser < Base
+      
+      include Configurable
+      
+      configurable_attr :file_command do `which file`.chomp end
+      
+      def mime_type(temp_object)
+        output = `#{file_command} -b --mime #{temp_object.path}`
+        output.split(';').first
+      end
+      
+    end
+    
+  end
+end
