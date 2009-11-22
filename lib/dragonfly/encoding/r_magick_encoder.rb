c230ce7191c4b54b344abc3ffaaed07428b8a4ec
Mark Evans <mark@new-bamboo.co.uk>
2009-11-22 21:13:07 +0000

    RMagickEncoder delegates encoding if it doesn't know how to

--- lib/dragonfly/encoding/r_magick_encoder.rb
+++ lib/dragonfly/encoding/r_magick_encoder.rb
@@ -5,7 +5,10 @@ module Dragonfly
     
     class RMagickEncoder < Base
       
+      SUPPORTED_FORMATS = Magick.formats.select{|k,v| v =~ /.*rw./ }.map{|f| f.first.downcase }
+      
       def encode(image, format, encoding={})
+        throw :unable_to_handle unless SUPPORTED_FORMATS.include?(format.to_s)
         encoded_image = Magick::Image.from_blob(image.data).first
         encoded_image.format = format.to_s
         encoded_image.to_blob
---
+++ spec/dragonfly/encoding/r_magick_encoder_spec.rb
@@ -0,0 +1,28 @@
+require File.dirname(__FILE__) + '/../../spec_helper'
+
+describe Dragonfly::Encoding::RMagickEncoder do
+  
+  before(:all) do
+    sample_file = File.dirname(__FILE__) + '/../../../samples/beach.png' # 280x355
+    @image = Dragonfly::TempObject.new(File.new(sample_file))
+    @encoder = Dragonfly::Encoding::RMagickEncoder.new
+  end
+  
+  describe "#encode" do
+    
+    it "should encode the image to the correct format" do
+      image = @encoder.encode(@image, :gif)
+      image.should have_format('gif')
+    end
+    
+    it "should throw :unable_to_handle if the format is not handleable" do
+      test_string = "I'm a string"
+      catch :unable_to_handle do
+        @encoder.encode(@image, :goofy)
+        test_string = "This line should not happen"
+      end
+      test_string.should == "I'm a string"
+    end
+  end
+  
+end
