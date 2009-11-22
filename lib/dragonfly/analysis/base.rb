2321da4b55f6e80ea8c3f27cb944ca5e1c832aed
Mark Evans <mark@new-bamboo.co.uk>
2009-11-22 21:21:27 +0000

    Methods in Encoding::Base and Analysis::Base weren't getting called because not part of public_methods(false) so get rid of them

--- lib/dragonfly/analysis/base.rb
+++ lib/dragonfly/analysis/base.rb
@@ -1,11 +1,6 @@
 module Dragonfly
   module Analysis
     class Base
-      
-      def mime_type(*args)
-        throw :unable_to_handle
-      end
-      
     end
   end
 end
--- lib/dragonfly/encoding/base.rb
+++ lib/dragonfly/encoding/base.rb
@@ -1,13 +1,6 @@
 module Dragonfly
-  module Encoding
-    
+  module Encoding    
     class Base
-      
-      def encode(*args)
-        throw :unable_to_handle
-      end
-      
     end
-    
   end
 end
