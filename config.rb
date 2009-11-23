148e361e1c038624fc8ae61429329e61c93ba251
Mark Evans <mark@new-bamboo.co.uk>
2009-11-23 00:35:32 +0000

    There's some strange problem which occurs when you include the Dragonfly module - not sure what it is, but best not to do it

--- config.rb
+++ config.rb
@@ -1,8 +1,6 @@
 require File.dirname(__FILE__) + '/lib/dragonfly'
 
-include Dragonfly
-
 APP = Dragonfly::App[:images]
-APP.configure_with(StandardConfiguration)
-APP.configure_with(RMagickConfiguration)
+APP.configure_with(Dragonfly::StandardConfiguration)
+APP.configure_with(Dragonfly::RMagickConfiguration)
 APP.url_handler.protect_from_dos_attacks = false
--- lib/dragonfly/delegatable.rb
+++ lib/dragonfly/delegatable.rb
@@ -6,7 +6,7 @@ module Dragonfly
         @delegatable_methods
       else
         ancestors = self.class.ancestors
-        @delegatable_methods = ancestors[0...ancestors.index(Dragonfly::Delegatable)].map{|i| i.instance_methods(false) }.flatten
+        @delegatable_methods = ancestors[0...ancestors.index(Delegatable)].map{|i| i.instance_methods(false) }.flatten
       end
     end
 
