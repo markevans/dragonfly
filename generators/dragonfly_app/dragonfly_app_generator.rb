cf28529fe7c434b6e883bdfff9462393d164d023
Mark Evans <mark@new-bamboo.co.uk>
2009-11-27 18:06:54 +0000

    Removed generated custom processor for rails generator - seems unnecessary

--- generators/dragonfly_app/dragonfly_app_generator.rb
+++ generators/dragonfly_app/dragonfly_app_generator.rb
@@ -7,7 +7,6 @@ class DragonflyAppGenerator < Rails::Generator::NamedBase
     metal_name = plural_name.camelize
     path_prefix = plural_name
     single_name = singular_name.singularize
-    custom_processor_name = "Custom#{single_name.camelize}Processing"
     
     record do |m|
       # The initializer
@@ -33,18 +32,7 @@ class DragonflyAppGenerator < Rails::Generator::NamedBase
           :app_name => app_name,
           :metal_name => metal_name,
           :path_prefix => path_prefix,
-          :random_secret => Digest::SHA1.hexdigest(Time.now.to_s),
-          :custom_processor_name => custom_processor_name
-        }
-      )
-      
-      # The custom processor
-      m.template(
-        'custom_processing.erb',
-        "lib/#{custom_processor_name.underscore}.rb",
-        :assigns => {
-          :module_name => custom_processor_name,
-          :temp_object_name => single_name
+          :random_secret => Digest::SHA1.hexdigest(Time.now.to_s)
         }
       )
       
