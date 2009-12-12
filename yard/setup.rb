d63f30ee406e8e2e26279afc8a01dce6c4427f1f
Mark Evans <mark@new-bamboo.co.uk>
2009-12-12 14:26:25 +0000

    Removed require ruby-debug - maybe this was breaking yard doc generation

--- yard/setup.rb
+++ yard/setup.rb
@@ -1,7 +1,3 @@
-# Allow debugging
-require 'rubygems'
-require 'ruby-debug'
-
 YARD::Templates::Engine.register_template_path(File.dirname(__FILE__) + '/templates')
 Dir[File.dirname(__FILE__) + '/handlers/*.rb'].each do |file|
   require File.expand_path(file)
