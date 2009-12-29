98d631c5cd703bbfc99ac3e2b23ea898fb37e540
Mark Evans <mark@new-bamboo.co.uk>
2009-12-29 17:47:53 +0000

    Fixed bin path problem in specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,6 +8,11 @@ require 'argument_matchers'
 require 'simple_matchers'
 require 'image_matchers'
 
+# A hack as system calls weren't using my path
+extra_paths = %w(/opt/local/bin)
+ENV['PATH'] ||= ''
+ENV['PATH'] += ':' + extra_paths.join(':')
+
 SAMPLES_DIR = File.expand_path(File.dirname(__FILE__) + '/../samples') unless defined?(SAMPLES_DIR)
 
 Spec::Runner.configure do |config|
