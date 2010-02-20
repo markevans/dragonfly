bee4b0d5eabdbeb6e00c412f3347492170300ccd
Mark Evans <mark@new-bamboo.co.uk>
2010-02-20 12:04:03 +0000

    Made Rails 3 test pending, because generators for cucumber/Rails templates don't seem to be working properly yet

--- features/rails_3.0.0.beta.feature
+++ features/rails_3.0.0.beta.feature
@@ -3,12 +3,13 @@ Feature: champion uses dragonfly in his Rails 3.0.0.beta application
   A user uses dragonfly in his Rails 3.0.0.beta application
 
   Background:
+    Given PENDING: Neither cucumber generators nor Rails templates seem to be working properly yet for Rails 3
     Given a Rails 3.0.0.beta application set up for using dragonfly
 
-  # Scenario: Set up dragonfly using the generator
-  #   When I use the Rails 3.0.0.beta generator to set up dragonfly
-  #   Then the cucumber features in my Rails 3.0.0.beta app should pass
+  Scenario: Set up dragonfly using the generator
+    When I use the Rails 3.0.0.beta generator to set up dragonfly
+    Then the cucumber features in my Rails 3.0.0.beta app should pass
 
   Scenario: Set up dragonfly using bundler require
     When I use the provided 3.0.0.beta initializer
-    # Then the cucumber features in my Rails 3.0.0.beta app should pass
+    Then the cucumber features in my Rails 3.0.0.beta app should pass
--- fixtures/rails_3.0.0.beta/template.rb
+++ fixtures/rails_3.0.0.beta/template.rb
@@ -1,20 +1,9 @@
-# generate 'scaffold albums cover_image_uid:string'
-# rake 'db:migrate'
+run "script/rails generate scaffold albums cover_image_uid:string"
+rake 'db:migrate'
 
-# # 'Vendor' dragonfly in the gems folder
-# dragonfly_dir = File.expand_path(File.dirname(__FILE__) + '/../../..')
-# run "mkdir -p vendor/gems && ln -s #{dragonfly_dir} vendor/gems/dragonfly-0.0.0"
-# 
-# # Avoid annoying warnings due to 'vendored' dragonfly gem
-# def insert_line(line, after_expr, file)
-#   run %(ruby -pe '$_ += "\\n#{line}\\n" if $_ =~ /#{after_expr}/' -i.bk #{file})
-# end
-# insert_line('Rails::VendorGemSourceIndex.silence_spec_warnings = true', '^require.*boot', 'config/environment.rb')
+gem 'cucumber'
+run 'script/rails generate cucumber:skeleton'
 
-# # Set up webrat and cucumber
-# gem 'cucumber'
-# generate 'cucumber'
-# 
 # # Copy over all files from the template dir
-# files_dir = File.expand_path(File.dirname(__FILE__) + '/../../files')
-# run "cp -r #{files_dir}/** ."
+files_dir = File.expand_path(File.dirname(__FILE__) + '/../../files')
+run "cp -r #{files_dir}/** ."
