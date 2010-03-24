9b6f5cf23ba15e51db1c7699ec75bd02c6c7c727
Mark Evans <mark@new-bamboo.co.uk>
2010-03-24 14:07:15 +0000

    Added marshal_encode/decode to serializer and made a bit quicker

--- lib/dragonfly/serializer.rb
+++ lib/dragonfly/serializer.rb
@@ -3,13 +3,23 @@ require 'base64'
 module Dragonfly
   module Serializer
     
+    extend self # So we can do Serializer.b64_encode, etc.
+    
     def b64_encode(string)
-      Base64.encode64(string).strip.sub(/=+$/,'')
+      Base64.encode64(string).tr("\n=",'')
     end
     
     def b64_decode(string)
       padding_length = string.length % 4
       Base64.decode64(string + '=' * padding_length)
+    end
+    
+    def marshal_encode(object)
+      b64_encode(Marshal.dump(object))
+    end
+    
+    def marshal_decode(string)
+      Marshal.load(b64_decode(string))
     end
     
   end
--- spec/dragonfly/serializer_spec.rb
+++ spec/dragonfly/serializer_spec.rb
@@ -21,4 +21,26 @@ describe Dragonfly::Serializer do
     end
   end
   
+  [
+    :hello,
+    nil,
+    true,
+    4,
+    2.3,
+    'wassup man',
+    [3,4,5],
+    {:wo => 'there'},
+    [{:this => 'should', :work => [3, 5.3, nil, {false => 'egg'}]}, [], true]
+  ].each do |object|
+    it "should correctly marshal encode #{object.inspect} properly with no padding/line break" do
+      encoded = marshal_encode(object)
+      # raise encoded.index("\n").inspect
+      encoded.should be_a(String)
+      encoded.should_not =~ /\n|=/
+    end
+    it "should correctly marshal encode and decode #{object.inspect} to the same object" do
+      marshal_decode(marshal_encode(object)).should == object
+    end
+  end
+  
 end
