f33377fc1331c66693ba912cfa2370990e2f168a
Mark Evans <mark@new-bamboo.co.uk>
2010-05-05 00:31:48 +0100

    SUPPORTED_FORMATS constant was proving problematic - use a configurable list instead

--- lib/dragonfly/encoding/r_magick_encoder.rb
+++ lib/dragonfly/encoding/r_magick_encoder.rb
@@ -2,14 +2,49 @@ require 'RMagick'
 
 module Dragonfly
   module Encoding
-    
+
     class RMagickEncoder < Base
-      
-      SUPPORTED_FORMATS = Magick.formats.select{|k,v| v =~ /.*rw./ }.map{|f| f.first.downcase }
-      
+
+      include Configurable
+
+      configurable_attr :supported_formats, [
+        :ai,
+        :bmp,
+        :eps,
+        :gif,
+        :gif87,
+        :ico,
+        :j2c,
+        :jp2,
+        :jpeg,
+        :jpg,
+        :pbm,
+        :pcd,
+        :pct,
+        :pcx,
+        :pdf,
+        :pict,
+        :pjpeg,
+        :png,
+        :png24,
+        :png32,
+        :png8,
+        :pnm,
+        :ppm,
+        :ps,
+        :psd,
+        :ras,
+        :tga,
+        :tiff,
+        :wbmp,
+        :xbm,
+        :xpm,
+        :xwd
+      ]
+
       def encode(image, format, encoding={})
         format = format.to_s.downcase
-        throw :unable_to_handle unless SUPPORTED_FORMATS.include?(format)
+        throw :unable_to_handle unless supported_formats.include?(format.to_sym)
         encoded_image = rmagick_image(image)
         if encoded_image.format.downcase == format
           image # do nothing
@@ -18,9 +53,9 @@ module Dragonfly
           encoded_image.to_blob
         end
       end
-      
+
       private
-      
+
       def rmagick_image(temp_object)
         Magick::Image.from_blob(temp_object.data).first
       rescue Magick::ImageMagickError => e
@@ -29,6 +64,6 @@ module Dragonfly
       end
 
     end
-    
+
   end
 end
