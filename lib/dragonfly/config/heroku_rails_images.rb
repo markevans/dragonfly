6209cf2c2e65c4d55613972e4cd405a520090857
Mark Evans <mark@new-bamboo.co.uk>
2010-05-04 22:52:01 +0100

    More helpful heroku rails config error message

--- lib/dragonfly/config/heroku_rails_images.rb
+++ lib/dragonfly/config/heroku_rails_images.rb
@@ -11,8 +11,8 @@ module Dragonfly
             c.datastore = Dragonfly::DataStorage::S3DataStore.new
             c.datastore.configure do |d|
               d.bucket_name = bucket_name
-              d.access_key_id = ENV['S3_KEY'] || raise("ENV variable 'S3_KEY' needs to be set")
-              d.secret_access_key = ENV['S3_SECRET'] || raise("ENV variable 'S3_SECRET' needs to be set")
+              d.access_key_id = ENV['S3_KEY'] || raise("ENV variable 'S3_KEY' needs to be set - use\n\theroku config:add S3_KEY=XXXXXXXXX")
+              d.secret_access_key = ENV['S3_SECRET'] || raise("ENV variable 'S3_SECRET' needs to be set - use\n\theroku config:add S3_SECRET=XXXXXXXXX")
             end
           end
         end
