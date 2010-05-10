776d925974ef47c2fffc7213141bb627252666ba
Mark Evans <mark@new-bamboo.co.uk>
2010-05-10 14:49:41 +0100

    Don't think you actually need use_filesystem=false for Heroku S3 config

--- lib/dragonfly/config/heroku_rails_images.rb
+++ lib/dragonfly/config/heroku_rails_images.rb
@@ -13,7 +13,6 @@ module Dragonfly
               d.bucket_name = bucket_name
               d.access_key_id = ENV['S3_KEY'] || raise("ENV variable 'S3_KEY' needs to be set - use\n\theroku config:add S3_KEY=XXXXXXXXX")
               d.secret_access_key = ENV['S3_SECRET'] || raise("ENV variable 'S3_SECRET' needs to be set - use\n\theroku config:add S3_SECRET=XXXXXXXXX")
-              d.use_filesystem = false
             end
           end
         end
