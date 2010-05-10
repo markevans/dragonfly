8b6cec298d95ded8465986b110e1d0255e596b45
Mark Evans <mark@new-bamboo.co.uk>
2010-05-10 17:06:03 +0100

    Show the Dragonfly version in the docs

--- yard/setup.rb
+++ yard/setup.rb
@@ -1,5 +1,15 @@
-YARD::Templates::Engine.register_template_path(File.dirname(__FILE__) + '/templates')
-Dir[File.dirname(__FILE__) + '/handlers/*.rb'].each do |file|
+this_dir = File.dirname(__FILE__)
+
+YARD::Templates::Engine.register_template_path(this_dir + '/templates')
+Dir[this_dir + '/handlers/*.rb'].each do |file|
   require File.expand_path(file)
 end
 YARD::Parser::SourceParser.parser_type = :ruby18
+
+version = ENV['DRAGONFLY_VERSION']
+DRAGONFLY_VERSION = if version
+  puts "Setting the version in the docs to #{version}"
+  version
+else
+  File.read(this_dir + '/../VERSION').strip
+end
