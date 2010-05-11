6e1f118bae2e268941b621f0aee45053b9d14360
Mark Evans <mark@new-bamboo.co.uk>
2010-05-11 15:02:31 +0100

    Oops wasn't using the correct processor in Config::RMagickText

--- lib/dragonfly/config/r_magick_text.rb
+++ lib/dragonfly/config/r_magick_text.rb
@@ -7,7 +7,7 @@ module Dragonfly
         app.configure do |c|
           c.datastore = DataStorage::TransparentDataStore.new
           c.register_analyser(Analysis::FileCommandAnalyser)
-          c.register_processor(Processing::RMagickProcessor)
+          c.register_processor(Processing::RMagickTextProcessor)
           c.register_encoder(Encoding::RMagickEncoder)
           c.parameters.add_shortcut :text, Hash do |_, options|
             {
