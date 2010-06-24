df89a8e4bbb689f687a9eb987bc664e79392d094
Mark Evans <mark@new-bamboo.co.uk>
2010-06-24 15:17:24 +0100

    Added ability for custom error message in validations

--- lib/dragonfly/active_record_extensions/validations.rb
+++ lib/dragonfly/active_record_extensions/validations.rb
@@ -15,8 +15,10 @@ module Dragonfly
         validates_each(*args) do |record, attr, attachment|
           if attachment
             property = attachment.send(property_name)
-            record.errors.add attr, "#{property_name.to_s.humanize.downcase} is incorrect. It needs to be #{expected_values_string(allowed_values)}," +
-                                    " but was '#{property}'" unless allowed_values.include?(property)
+            record.errors.add(attr,
+                              opts[:message] ||
+                                "#{property_name.to_s.humanize.downcase} is incorrect. It needs to be #{expected_values_string(allowed_values)}, but was '#{property}'"
+                              ) unless allowed_values.include?(property)
           end
         end
 
