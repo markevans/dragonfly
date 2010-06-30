4e59cf00bb51e79619e090a623b459dd0f619b6f
Mark Evans <mark@new-bamboo.co.uk>
2010-06-30 14:56:30 +0100

    Oops typo

--- lib/dragonfly/analyser_list.rb
+++ lib/dragonfly/analyser_list.rb
@@ -7,7 +7,7 @@ module Dragonfly
     end
     
     def analyse(temp_object, method, *args)
-      delegate(meth, temp_object, *args)
+      delegate(method, temp_object, *args)
     end
     
   end
