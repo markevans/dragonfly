bd4fad3f235ff0c341b613e5534ba82fdbaa2fd8
Mark Evans <mark@new-bamboo.co.uk>
2010-08-07 17:52:58 +0100

    Mongo tests are pending if mongo not running

--- spec/dragonfly/data_storage/mongo_data_store_spec.rb
+++ spec/dragonfly/data_storage/mongo_data_store_spec.rb
@@ -1,9 +1,15 @@
 require File.dirname(__FILE__) + '/../../spec_helper'
 require File.dirname(__FILE__) + '/data_store_spec'
+require 'mongo'
 
 describe Dragonfly::DataStorage::MongoDataStore do
   
   before(:each) do
+    begin
+      Mongo::Connection.new
+    rescue Mongo::ConnectionFailure => e
+      pending "You need to start mongo on localhost:27017 to test the MongoDataStore"
+    end
     @data_store = Dragonfly::DataStorage::MongoDataStore.new
   end
   
