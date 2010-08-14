a52b6f279aafce4b8dfd3b85db76e85a64ec2e9b
Mark Evans <mark@new-bamboo.co.uk>
2010-08-14 17:02:53 +0100

    Oops Heroku config wasn't actually configuring

--- lib/dragonfly/config/heroku.rb
+++ lib/dragonfly/config/heroku.rb
@@ -4,11 +4,13 @@ module Dragonfly
     module Heroku
 
       def self.apply_configuration(app, bucket_name)
-        c.datastore = Dragonfly::DataStorage::S3DataStore.new
-        c.datastore.configure do |d|
-          d.bucket_name = bucket_name
-          d.access_key_id = ENV['S3_KEY'] || raise("ENV variable 'S3_KEY' needs to be set - use\n\theroku config:add S3_KEY=XXXXXXXXX")
-          d.secret_access_key = ENV['S3_SECRET'] || raise("ENV variable 'S3_SECRET' needs to be set - use\n\theroku config:add S3_SECRET=XXXXXXXXX")
+        app.configure do |c|
+          c.datastore = Dragonfly::DataStorage::S3DataStore.new
+          c.datastore.configure do |d|
+            d.bucket_name = bucket_name
+            d.access_key_id = ENV['S3_KEY'] || raise("ENV variable 'S3_KEY' needs to be set - use\n\theroku config:add S3_KEY=XXXXXXXXX")
+            d.secret_access_key = ENV['S3_SECRET'] || raise("ENV variable 'S3_SECRET' needs to be set - use\n\theroku config:add S3_SECRET=XXXXXXXXX")
+          end
         end
       end
 
