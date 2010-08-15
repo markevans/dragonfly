1d2c249753732051b5590624a87e141b2760b027
Mark Evans <mark@new-bamboo.co.uk>
2010-08-15 20:19:45 +0100

    Attachment just uses app.store now

--- spec/dragonfly/active_model_extensions/model_spec.rb
+++ spec/dragonfly/active_model_extensions/model_spec.rb
@@ -102,7 +102,7 @@ describe Item do
         @item.preview_image_uid.should be_nil
       end
       it "should store the image when saved" do
-        @app.datastore.should_receive(:store).with(a_temp_object_with_data("DATASTRING"))
+        @app.datastore.should_receive(:store).with(a_temp_object_with_data("DATASTRING"), {})
         @item.save!
       end
       it "should not try to destroy anything on destroy" do
@@ -136,7 +136,7 @@ describe Item do
 
       before(:each) do
         @item.preview_image = "DATASTRING"
-        @app.datastore.should_receive(:store).with(a_temp_object_with_data("DATASTRING")).once.and_return('some_uid')
+        @app.datastore.should_receive(:store).with(a_temp_object_with_data("DATASTRING"), {}).once.and_return('some_uid')
         @item.save!
       end
 
@@ -203,7 +203,7 @@ describe Item do
           @item.save!
         end
         it "should store the new data when saved" do
-          @app.datastore.should_receive(:store).with(a_temp_object_with_data("ANEWDATASTRING"))
+          @app.datastore.should_receive(:store).with(a_temp_object_with_data("ANEWDATASTRING"), {})
           @item.save!
         end
         it "should destroy the old data on destroy" do
