a7c190cf80686b4117417c33ee799022482e4c74
Mark Evans <mark@new-bamboo.co.uk>
2010-08-28 09:53:47 +0100

    Changed irbrc a bit

--- irbrc.rb
+++ irbrc.rb
@@ -1,21 +1,15 @@
 require File.dirname(__FILE__) + '/lib/dragonfly'
 APP = Dragonfly[:images].configure_with(:rmagick)
 
-available_uids = `find #{APP.datastore.root_path} ! -type d`.split("\n").map do |file|
-  file.sub("#{APP.datastore.root_path}/", '')
-end
-
-def new_image
-  APP.create_object(File.new(Dir['samples/*'].first))
-end
+# available_uids = `find #{APP.datastore.root_path} ! -type d`.split("\n").map do |file|
+#   file.sub("#{APP.datastore.root_path}/", '')
+# end
 
 puts "Loaded stuff from dragonfly irbrc"
-puts "\nAvailable uids:\n"
-puts available_uids
+# puts "\nAvailable uids:\n"
+# puts available_uids
 puts "\nAvailable sample images:\n"
 puts Dir['samples/*']
-puts "\nAvailable methods:\n"
-puts "new_image"
 puts "\nAvailable constants:\n"
 puts "APP"
 puts
