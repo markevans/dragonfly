abeaa8eee61b8c494fca61d8713264a9e11261d4
Mark Evans <mark@new-bamboo.co.uk>
2010-10-24 14:01:38 +0100

    Added authentication to mongo data store

--- spec/dragonfly/data_storage/mongo_data_store_spec.rb
+++ spec/dragonfly/data_storage/mongo_data_store_spec.rb
@@ -10,9 +10,28 @@ describe Dragonfly::DataStorage::MongoDataStore do
     rescue Mongo::ConnectionFailure => e
       pending "You need to start mongo on localhost:27017 to test the MongoDataStore"
     end
-    @data_store = Dragonfly::DataStorage::MongoDataStore.new
+    @data_store = Dragonfly::DataStorage::MongoDataStore.new :database => 'dragonfly_test'
   end
   
   it_should_behave_like 'data_store'
+  
+  describe "authenticating" do
+    before(:each) do
+      @temp_object = Dragonfly::TempObject.new('Feijão verde')
+    end
+    
+    it "should not attempt to authenticate if a username is not given" do
+      @data_store.db.should_not_receive(:authenticate)
+      @data_store.store(@temp_object)    
+    end
+    
+    it "should attempt to authenticate once if a username is given" do
+      @data_store.username = 'terry'
+      @data_store.password = 'butcher'
+      @data_store.db.should_receive(:authenticate).exactly(:once).with('terry','butcher').and_return(true)
+      uid = @data_store.store(@temp_object)
+      @data_store.retrieve(uid)
+    end
+  end
 
 end
