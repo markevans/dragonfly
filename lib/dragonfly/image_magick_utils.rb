5e60312df9d3d1e18cdff3a7397da1395539522a
Philip Arndt <parndt@gmail.com>
2010-11-22 14:15:52 +1300

    Reintroduce a quiet identify because it's driving me crazy seeing output from ImageMagick when running Cucumber on my project.

--- lib/dragonfly/image_magick_utils.rb
+++ lib/dragonfly/image_magick_utils.rb
@@ -24,19 +24,28 @@ module Dragonfly
     end
 
     def identify(temp_object)
-      # example of details string:
-      # myimage.png PNG 200x100 200x100+0+0 8-bit DirectClass 31.2kb
-      details = raw_identify(temp_object)
-      filename, format, geometry, geometry_2, depth, image_class, size = details.split(' ')
-      width, height = geometry.split('x')
-      {
-        :filename => filename,
-        :format => format.downcase.to_sym,
-        :width => width.to_i,
-        :height => height.to_i,
-        :depth => depth.to_i,
-        :image_class => image_class
-      }
+      begin
+        original_stderr = $stderr.dup
+        tempfile = Tempfile.new('stderr')
+        $stderr.reopen(tempfile)
+        # example of details string:
+        # myimage.png PNG 200x100 200x100+0+0 8-bit DirectClass 31.2kb
+        details = raw_identify(temp_object)
+        filename, format, geometry, geometry_2, depth, image_class, size = details.split(' ')
+        width, height = geometry.split('x')
+        {
+          :filename => filename,
+          :format => format.downcase.to_sym,
+          :width => width.to_i,
+          :height => height.to_i,
+          :depth => depth.to_i,
+          :image_class => image_class
+        }
+      rescue ShellCommandFailed
+        throw :unable_to_handle
+      ensure
+        $stderr.reopen(original_stderr)
+      end
     end
     
     def raw_identify(temp_object, args='')
