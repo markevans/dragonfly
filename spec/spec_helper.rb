eb08383f1151cb0775aadac24920fa09c3019333
Mark Evans <mark@new-bamboo.co.uk>
2011-02-19 12:52:10 +0000

    ensure bundler is used for specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,7 @@
+require "rubygems"
+require "bundler"
+Bundler.setup(:default, :test)
+
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'rspec'
