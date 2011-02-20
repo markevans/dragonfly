85ca646bc3d164db8a56f355a9877bf248c7aca6
Mark Evans <mark@new-bamboo.co.uk>
2011-02-20 18:17:23 +0000

    no need for eggs

--- spec/dragonfly/analysis/image_magick_analyser_spec.rb
+++ spec/dragonfly/analysis/image_magick_analyser_spec.rb
@@ -7,7 +7,6 @@ describe Dragonfly::Analysis::ImageMagickAnalyser do
     image_path = File.dirname(__FILE__) + '/../../../samples/beach.png'
     @image = Dragonfly::TempObject.new(File.new(image_path))
     @analyser = Dragonfly::Analysis::ImageMagickAnalyser.new
-    @analyser.log = Logger.new(LOG_FILE)
   end
 
   it_should_behave_like "image analyser methods"
