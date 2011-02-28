4b4eb601aeb5d7e2efd6781169c5fc382aa8d9f0
Mark Evans <mark@new-bamboo.co.uk>
2011-02-28 17:34:44 +0000

    Got url_mapper doing what I want it to

--- lib/dragonfly/url_mapper.rb
+++ lib/dragonfly/url_mapper.rb
@@ -3,28 +3,32 @@ module Dragonfly
     
     # Exceptions
     class MissingParams < StandardError; end
+    class BadUrlFormat < StandardError; end
     
-    PARAM_FORMAT = /\:[\w_]+/
+    DEFAULT_SEGMENT_PATTERN = '[\w_]?'
     
-    def initialize(url_format)
+    def initialize(url_format, segment_patterns={})
       @url_format = url_format
+      @segment_patterns = segment_patterns
+      generate_url_regexp
     end
 
-    attr_reader :url_format
+    attr_reader :url_format, :url_regexp, :segment_patterns
     
     def params_for(url)
       path, query = url.split('?')
       if path and md = path.match(url_regexp)
         params = Rack::Utils.parse_query(query)
-        required_params.each_with_index do |var, i|
-          params[var] = md[i+1]
+        params_in_url.each_with_index do |var, i|
+          value = md[i+1][1..-1] if md[i+1]
+          params[var] = value
         end
         params
       end
     end
 
-    def required_params
-      @required_params ||= url_format.scan(PARAM_FORMAT).map{|f| f.tr(':','') }
+    def params_in_url
+      @params_in_url ||= url_format.scan(/\:[\w_]+/).map{|f| f.tr(':','') }
     end
     
     def url_for(params)
@@ -35,13 +39,25 @@ module Dragonfly
       end
       url << "?#{Rack::Utils.build_query(params)}" if params.any?
       if url[':']
-        raise MissingParams, "missing params #{url.scan(PARAM_FORMAT).join(', ')}"
+        raise MissingParams, "missing params #{url.scan(/\:[\w_]+/).join(', ')}"
       end
       url
     end
     
-    def url_regexp
-      @url_regexp ||= Regexp.new('^' + url_format.gsub(PARAM_FORMAT, '([\w_]+)') + '$')
+    private
+    
+    def generate_url_regexp
+      raise BadUrlFormat, "bad url format #{url_format}" if url_format[/[\w_]:[\w_]/]
+      regexp_string = url_format.gsub(/([^\w_]):([\w_]+)/) do
+        pattern = segment_patterns[$2.to_sym] || DEFAULT_SEGMENT_PATTERN
+        seperator = Regexp.escape($1)
+        if pattern[-1..-1] == '?'
+          "(#{seperator}#{pattern[0..-2]}+?)?"
+        else
+          "(#{seperator}#{pattern}+?)"
+        end
+      end
+      @url_regexp = Regexp.new('^' + regexp_string + '$')
     end
     
   end
--- spec/dragonfly/url_mapper_spec.rb
+++ spec/dragonfly/url_mapper_spec.rb
@@ -2,10 +2,38 @@ require 'spec_helper'
 
 describe Dragonfly::UrlMapper do
 
-  describe "required_params" do
+  describe "validating the url format" do
+    it "should be ok with a valid one" do
+      Dragonfly::UrlMapper.new('/media/:job/:name')
+    end
+    it "should throw an error if params aren't separated" do
+      lambda{
+        Dragonfly::UrlMapper.new('/media/:job:name')
+      }.should raise_error(Dragonfly::UrlMapper::BadUrlFormat)
+    end
+  end
+
+  describe "params_in_url" do
     it "should return everything specified in the url" do
       url_mapper = Dragonfly::UrlMapper.new('/media/:job/:basename.:ext')
-      url_mapper.required_params.should == ['job', 'basename', 'ext']
+      url_mapper.params_in_url.should == ['job', 'basename', 'ext']
+    end
+  end
+  
+  describe "url_regexp" do
+    it "should return a regexp with non-greedy optional groups that include the preceding slash/dot/dash" do
+      url_mapper = Dragonfly::UrlMapper.new('/media/:job/:basename-:size.:format')
+      url_mapper.url_regexp.should == %r{^/media(/[\w_]+?)?(/[\w_]+?)?(\-[\w_]+?)?(\.[\w_]+?)?$}
+    end
+    
+    it "should allow setting custom patterns in the url" do
+      url_mapper = Dragonfly::UrlMapper.new('/media/:job-:size.:format', :job => '\w', :size => '\d', :format => '[^\.]')
+      url_mapper.url_regexp.should == %r{^/media(/\w+?)(\-\d+?)(\.[^\.]+?)$}
+    end
+    
+    it "should make optional match patterns (ending in ?) apply to the whole group including the preceding seperator" do
+      url_mapper = Dragonfly::UrlMapper.new('/media/:job', :job => '\w?')
+      url_mapper.url_regexp.should == %r{^/media(/\w+?)?$}
     end
   end
 
@@ -41,23 +69,39 @@ describe Dragonfly::UrlMapper do
     it "should include query parameters" do
       @url_mapper.params_for('/media/asdf?when=now').should == {'job' => 'asdf', 'when' => 'now'}
     end
+  end
 
-    [
-      '',
-      '/',
-      '/media',
-      '/media/',
-      '/moodia/asdf',
-      '/media/asdf/',
-      '/media/asdf/stuff',
-      '/mount/media/asdf'
-    ].each do |url|
-      it "should return nil if the url doesn't match, e.g. #{url}" do
-        @url_mapper.params_for(url).should be_nil
+  describe "matching urls with standard format /media/:job/:basename.:format" do
+    before(:each) do
+      @url_mapper = Dragonfly::UrlMapper.new('/media/:job/:basename.:format',
+        :job => '\w',
+        :basename => '[^\/]?',
+        :format => '[^\.]?'
+      )
+    end
+
+    {
+      '' => nil,
+      '/' => nil,
+      '/media' => nil,
+      '/media/' => nil,
+      '/moodia/asdf' => nil,
+      '/media/asdf/' => nil,
+      '/mount/media/asdf' => nil,
+      '/media/asdf/stuff.egg' => {'job' => 'asdf', 'basename' => 'stuff', 'format' => 'egg'},
+      '/media/asdf' =>           {'job' => 'asdf', 'basename' => nil,     'format' => nil},
+      '/media/asdf/stuff' =>     {'job' => 'asdf', 'basename' => 'stuff', 'format' => nil},
+      '/media/asdf.egg' =>       {'job' => 'asdf', 'basename' => nil,     'format' => 'egg'},
+      '/media/asdf/stuff/egg' => nil,
+      '/media/asdf/stuff.dog.egg' => {'job' => 'asdf', 'basename' => 'stuff.dog', 'format' => 'egg'},
+      '/media/asdf/s=2 -.d.e' => {'job' => 'asdf', 'basename' => 's=2 -.d', 'format' => 'e'},
+      '/media/asdf-40x40/stuff.egg' => nil
+    }.each do |url, params|
+      it "should return the url #{url} into params #{params.inspect}" do
+        @url_mapper.params_for(url).should == params
       end    
     end
-    
-    
+
   end
   
 end
