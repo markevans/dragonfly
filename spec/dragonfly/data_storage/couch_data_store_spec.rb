6123915882f4c0a932302389242ef3c1bc9db55a
Mark Evans <mark@new-bamboo.co.uk>
2011-04-06 13:37:11 +0100

    Added tests for serving from couchdb and allowed setting content-type on store

--- spec/dragonfly/data_storage/couch_data_store_spec.rb
+++ spec/dragonfly/data_storage/couch_data_store_spec.rb
@@ -1,6 +1,7 @@
 # encoding: utf-8
 require 'spec_helper'
 require File.dirname(__FILE__) + '/shared_data_store_examples'
+require 'net/http'
 
 describe Dragonfly::DataStorage::CouchDataStore do
 
@@ -23,5 +24,47 @@ describe Dragonfly::DataStorage::CouchDataStore do
   end
   
   it_should_behave_like 'data_store'
+  
+  describe "serving from couchdb" do
+
+    def get_content(uid, name)
+      Net::HTTP.start('localhost', 5984) {|http|
+        http.get("/dragonfly_test/#{uid}/#{name}")
+      }
+    end
+
+    before(:each) do
+      @temp_object = Dragonfly::TempObject.new('testingyo')
+    end
+
+    it "should serve using the format of the filename" do
+      pending
+    end
+    
+    it "should use the fallback if it has no ext" do
+      pending
+    end
+    
+    it "should use the fallback by default" do
+      uid = @data_store.store(@temp_object)
+      response = get_content(uid, 'file')
+      response.body.should == 'testingyo'
+      response['Content-Type'].should == 'application/octet-stream'
+    end
+    
+    it "should allow setting on store with 'content_type'" do
+      uid = @data_store.store(@temp_object, :content_type => 'text/plain')
+      response = get_content(uid, 'file')
+      response.body.should == 'testingyo'
+      response['Content-Type'].should == 'text/plain'
+    end
+    
+    it "should allow setting on store with 'mime_type'" do
+      uid = @data_store.store(@temp_object, :mime_type => 'text/plain-yo')
+      response = get_content(uid, 'file')
+      response.body.should == 'testingyo'
+      response['Content-Type'].should == 'text/plain-yo'
+    end
+  end
   
 end
