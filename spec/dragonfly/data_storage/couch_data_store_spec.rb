689e308a2a31ad6a95b4c12082e382428a025f10
Mark Evans <mark@new-bamboo.co.uk>
2011-04-06 11:23:35 +0100

    Set couch tests to pending when couch isn't being run

--- spec/dragonfly/data_storage/couch_data_store_spec.rb
+++ spec/dragonfly/data_storage/couch_data_store_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 require File.dirname(__FILE__) + '/shared_data_store_examples'
 
 describe Dragonfly::DataStorage::CouchDataStore do
+
   before(:each) do
     WebMock.allow_net_connect!
     @data_store = Dragonfly::DataStorage::CouchDataStore.new(
@@ -12,6 +13,13 @@ describe Dragonfly::DataStorage::CouchDataStore do
       :password => "", 
       :database => "dragonfly_test"
     )
+    begin
+      @data_store.db.get('ping')
+    rescue Errno::ECONNREFUSED => e
+      pending "You need to start CouchDB on localhost:5984 to test the CouchDataStore"
+    rescue RestClient::ResourceNotFound
+    end
+    
   end
   
   it_should_behave_like 'data_store'
