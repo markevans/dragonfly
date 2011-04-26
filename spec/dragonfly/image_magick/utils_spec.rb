f6c05a58ca0c76d8cf8c497d6b900f1f1979f64e
Aidan Feldman <aidan@jux.com>
2011-04-26 01:57:41 -0400

    added spec for commands with parens

--- spec/dragonfly/image_magick/utils_spec.rb
+++ spec/dragonfly/image_magick/utils_spec.rb
@@ -15,4 +15,8 @@ describe Dragonfly::ImageMagick::Utils do
     end
   end
 
+  it "should work for commands with parenthesis" do
+    expect { @obj.send(:run, "\\( +clone -sparse-color Barycentric '0,0 black 0,%[fx:h-1] white' -function polynomial 2,-2,0.5 \\) -compose Blur -set option:compose:args 15 -composite") }.to_not raise_error
+  end
+
 end
