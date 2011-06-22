0ab5875db74b32c9f7f19a69027df29d5bbde3b1
Cyril Mougel <cyril.mougel@gmail.com>
2011-06-22 13:58:53 +0200

    define a content-type directly in GridFS

--- spec/dragonfly/data_storage/mongo_data_store_spec.rb
+++ spec/dragonfly/data_storage/mongo_data_store_spec.rb
@@ -4,7 +4,7 @@ require File.dirname(__FILE__) + '/shared_data_store_examples'
 require 'mongo'
 
 describe Dragonfly::DataStorage::MongoDataStore do
-  
+
   before(:each) do
     begin
       Mongo::Connection.new
@@ -13,19 +13,19 @@ describe Dragonfly::DataStorage::MongoDataStore do
     end
     @data_store = Dragonfly::DataStorage::MongoDataStore.new :database => 'dragonfly_test'
   end
-  
+
   it_should_behave_like 'data_store'
-  
+
   describe "authenticating" do
     before(:each) do
       @temp_object = Dragonfly::TempObject.new('Feijão verde')
     end
-    
+
     it "should not attempt to authenticate if a username is not given" do
       @data_store.db.should_not_receive(:authenticate)
-      @data_store.store(@temp_object)    
+      @data_store.store(@temp_object)
     end
-    
+
     it "should attempt to authenticate once if a username is given" do
       @data_store.username = 'terry'
       @data_store.password = 'butcher'
@@ -40,17 +40,30 @@ describe Dragonfly::DataStorage::MongoDataStore do
       @connection = Mongo::Connection.new
       @temp_object = Dragonfly::TempObject.new('asdf')
     end
-    
+
     it "should allow sharing the connection" do
       @data_store.connection = @connection
       @connection.should_receive(:db).with('dragonfly_test').and_return(db=mock)
       @data_store.db.should == db
     end
-    
+
     it "should allow sharing the db" do
       db = @connection.db('dragonfly_test_yo')
       @data_store.db = db
       @data_store.grid.instance_eval{@db}.should == db # so wrong
+    end
+  end
+
+  describe "serving from mongodb" do
+
+    before(:each) do
+      @temp_object = Dragonfly::TempObject.new('testingyo')
+    end
+
+    it "should allow setting on store with 'content_type'" do
+      uid = @data_store.store(@temp_object, :content_type => 'text/plain')
+      @data_store.grid.get(BSON::ObjectId(uid)).content_type.should == 'text/plain'
+      @data_store.grid.get(BSON::ObjectId(uid)).read.should == 'testingyo'
     end
   end
 
