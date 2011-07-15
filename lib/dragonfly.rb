20e90b220e8c46a940f7e706fcbeb0b41539da0f
Josh Adams <josh@isotope11.com>
2011-07-14 23:57:36 -0500

    [rackspace] Add rackspace configuration key

--- lib/dragonfly.rb
+++ lib/dragonfly.rb
@@ -51,6 +51,7 @@ module Dragonfly
     App.register_configuration(:image_magick){ ImageMagick::Config }
     App.register_configuration(:rails){ Config::Rails }
     App.register_configuration(:heroku){ Config::Heroku }
+    App.register_configuration(:rackspace){ Config::Rackspace }
 
   end
 end
