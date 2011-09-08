8912e5bf4bb73a7f12be1a5126f451ad092b5919
James McKinney <james@slashpoundbang.com>
2011-09-08 19:10:23 -0400

    propagate default_configuration to fallback_configurable

--- lib/dragonfly/configurable.rb
+++ lib/dragonfly/configurable.rb
@@ -80,7 +80,10 @@ module Dragonfly
         child_configurables << obj
         config_methods.push(*obj.config_methods)
         default_configuration.merge!(obj.default_configuration)
-        fallback_configurable.config_methods.push(*obj.config_methods) if fallback_configurable
+        if fallback_configurable
+          fallback_configurable.config_methods.push(*obj.config_methods)
+          fallback_configurable.default_configuration.merge!(obj.default_configuration)
+        end
       end
 
       def set_if_unset(key, value)
