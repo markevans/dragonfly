35da7071c5ed9c6c53a701c06d0c0c38ebcddd01
Mark Evans <mark@new-bamboo.co.uk>
2011-11-11 12:41:08 +0000

    Made camelize and autoload_files_in_dir procs, not methods, so that they're not defined on Object everywhere

--- lib/dragonfly.rb
+++ lib/dragonfly.rb
@@ -3,7 +3,7 @@
 # The convention is that dirs are modules
 # so declare them here and autoload any modules/classes inside them
 # All paths here are absolute
-def camelize(path)
+camelize = proc do |path|
   # e.g. 'test/this_one' => Test::ThisOne
   "#{path}".
     chomp('/').
@@ -12,23 +12,24 @@ def camelize(path)
     gsub('_','').
     sub(/^(\w)/){ $1.upcase }
 end
-def autoload_files_in_dir(path, namespace)
+
+autoload_files_in_dir = proc do |path, namespace|
   # Define the module
   eval("module #{namespace}; end")
   # Autoload modules/classes in that module
   Dir.glob("#{path}/*.rb").each do |file|
     file = File.expand_path(file)
-    sub_const_name = camelize( File.basename(file, '.rb') )
+    sub_const_name = camelize[ File.basename(file, '.rb') ]
     eval("#{namespace}.autoload('#{sub_const_name}', '#{file}')")
   end
   # Recurse on subdirectories
   Dir.glob("#{path}/*/").each do |dir|
-    sub_namespace = camelize( File.basename(dir) )
-    autoload_files_in_dir(dir, "#{namespace}::#{sub_namespace}")
+    sub_namespace = camelize[ File.basename(dir) ]
+    autoload_files_in_dir[dir, "#{namespace}::#{sub_namespace}"]
   end
 end
 
-autoload_files_in_dir("#{File.dirname(__FILE__)}/dragonfly", 'Dragonfly')
+autoload_files_in_dir["#{File.dirname(__FILE__)}/dragonfly", 'Dragonfly']
 
 require 'dragonfly/core_ext/object'
 require 'dragonfly/core_ext/array'
