81821c7b221f0a37fe0e70b08eb1bf1156a58a73
Mark Evans <mark@new-bamboo.co.uk>
2011-12-28 17:30:49 +0000

    Made use of Rails 3 Validators - more maintainable code

--- lib/dragonfly/active_model_extensions/validations.rb
+++ lib/dragonfly/active_model_extensions/validations.rb
@@ -2,42 +2,65 @@ module Dragonfly
   module ActiveModelExtensions
     module Validations
 
-      private
-
-      def validates_property(property_name, opts)
-        attrs = opts[:of] or raise ArgumentError, "you need to provide the attribute which has the property, using :of => <attribute_name>"
-        attrs = [attrs].flatten #(make sure it's an array)
-
-        raise ArgumentError, "you must provide either :in => [<value1>, <value2>..] or :as => <value>" unless opts[:in] || opts[:as]
-        allowed_values = opts[:in] || [opts[:as]]
-
-        args = attrs + [opts]
-        validates_each(*args) do |model, attr, attachment|
+      class PropertyValidator < ActiveModel::EachValidator
+        
+        def validate_each(model, attribute, attachment)
           if attachment
             property = attachment.send(property_name)
-            if opts[:case_sensitive] == false
-              allowed_values = allowed_values.map{|entry| entry.to_s.downcase}
-              property = property.to_s.downcase
-            end
-            unless allowed_values.include?(property)
-              message = opts[:message] ||
-                "#{property_name.to_s.humanize.downcase} is incorrect. "+
-                "It needs to be #{Validations.expected_values_string(allowed_values)}"+
-                (property ? ", but was '#{property}'" : "")
-              message = message.call(property, model) if message.respond_to?(:call)
-              model.errors.add(attr, message)
-            end
+            model.errors.add(attribute, message(property, model)) unless matches?(property)
           end
         end
-
+        
+        private
+        
+        def matches?(property)
+          if case_insensitive?
+            prop = property.to_s.downcase
+            allowed_values.any?{|v| v.to_s.downcase == prop }
+          else
+            allowed_values.include?(property)
+          end
+        end
+        
+        def message(property, model)
+          message = options[:message] ||
+            "#{property_name.to_s.humanize.downcase} is incorrect. " +
+            "It needs to be #{expected_values_string}" +
+            (property ? ", but was '#{property}'" : "")
+          message.respond_to?(:call) ? message.call(property, model) : message
+        end
+        
+        def check_validity!
+          raise ArgumentError, "you must provide either :in => [<value1>, <value2>..] or :as => <value>" unless options[:in] || options[:as]
+        end
+        
+        def property_name
+          options[:property_name]
+        end
+        
+        def case_insensitive?
+          options[:case_sensitive] == false
+        end
+        
+        def allowed_values
+          @allowed_values ||= options[:in] || [options[:as]]
+        end
+        
+        def expected_values_string
+          if allowed_values.is_a?(Range)
+            "between #{allowed_values.first} and #{allowed_values.last}"
+          else
+            allowed_values.length > 1 ? "one of '#{allowed_values.join('\', \'')}'" : "'#{allowed_values.first.to_s}'"
+          end
+        end
+        
       end
 
-      def self.expected_values_string(allowed_values)
-        if allowed_values.is_a?(Range)
-          "between #{allowed_values.first} and #{allowed_values.last}"
-        else
-          allowed_values.length > 1 ? "one of '#{allowed_values.join('\', \'')}'" : "'#{allowed_values.first.to_s}'"
-        end
+      private
+
+      def validates_property(property_name, options)
+        raise ArgumentError, "you need to provide the attribute which has the property, using :of => <attribute_name>" unless options[:of]
+        validates_with PropertyValidator, options.merge(:attributes => [*options[:of]], :property_name => property_name)
       end
 
     end
