03881b698193113a9e117bfe7a63e0869b39cbef
Mark Evans <mark@new-bamboo.co.uk>
2012-08-13 18:10:04 +0100

    Added protect_from_dos_attacks and secret to the rails generator

--- lib/rails/generators/dragonfly/dragonfly_generator.rb
+++ lib/rails/generators/dragonfly/dragonfly_generator.rb
@@ -2,6 +2,12 @@ class DragonflyGenerator < Rails::Generators::Base
   source_root File.expand_path('../templates', __FILE__)
   
   def create_initializer
-    template "initializer.rb", "config/initializers/dragonfly.rb"
+    template "initializer.rb.erb", "config/initializers/dragonfly.rb"
+  end
+  
+  private
+  
+  def generate_secret
+    rand(1e32).to_s(36)
   end
 end
