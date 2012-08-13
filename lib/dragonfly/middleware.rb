101fe351cc78248904d74d9bf1ca159b85d0dc5e
Mark Evans <mark@new-bamboo.co.uk>
2012-08-13 17:52:35 +0100

    Dragonfly::Middleware uses the default_app by default

--- lib/dragonfly/middleware.rb
+++ lib/dragonfly/middleware.rb
@@ -1,13 +1,14 @@
 module Dragonfly
   class Middleware
 
-    def initialize(app, dragonfly_app_name)
+    def initialize(app, dragonfly_app_name=nil)
       @app = app
       @dragonfly_app_name = dragonfly_app_name
     end
 
     def call(env)
-      response = Dragonfly[@dragonfly_app_name].call(env)
+      dragonfly_app = @dragonfly_app_name ? Dragonfly[@dragonfly_app_name] : Dragonfly.default_app
+      response = dragonfly_app.call(env)
       if response[1]['X-Cascade'] == 'pass'
         @app.call(env)
       else
--- spec/dragonfly/middleware_spec.rb
+++ spec/dragonfly/middleware_spec.rb
@@ -11,37 +11,57 @@ describe Dragonfly::Middleware do
     Rack::MockRequest.new(app).get(url)
   end
 
-  before(:each) do
-    @stack = Rack::Builder.new do
-      use Dragonfly::Middleware, :images
-      run dummy_rack_app
+  describe "using the default app" do
+    before(:each) do
+      @stack = Rack::Builder.new do
+        use Dragonfly::Middleware
+        run dummy_rack_app
+      end
+    end
+
+    it "should pass through if the app returns X-Cascade: pass" do
+      Dragonfly.default_app.should_receive(:call).and_return(
+        [404, {"Content-Type" => 'text/plain', 'X-Cascade' => 'pass'}, ['Not found']]
+      )
+      response = make_request(@stack, '/media/hello.png?howare=you')
+      response.body.should == 'dummy_rack_app body'
+      response.status.should == 200
+    end
+
+    it "should return a 404 if the app returns a 404" do
+      Dragonfly.default_app.should_receive(:call).and_return(
+        [404, {"Content-Type" => 'text/plain'}, ['Not found']]
+      )
+      response = make_request(@stack, '/media/hello.png?howare=you')
+      response.status.should == 404
+    end
+
+    it "should return as per the dragonfly app if the app returns a 200" do
+      Dragonfly.default_app.should_receive(:call).and_return(
+        [200, {"Content-Type" => 'text/plain'}, ['ABCD']]
+      )
+      response = make_request(@stack, '/media/hello.png?howare=you')
+      response.status.should == 200
+      response.body.should == 'ABCD'
     end
   end
 
-  it "should pass through if the app returns X-Cascade: pass" do
-    Dragonfly[:images].should_receive(:call).and_return(
-      [404, {"Content-Type" => 'text/plain', 'X-Cascade' => 'pass'}, ['Not found']]
-    )
-    response = make_request(@stack, '/media/hello.png?howare=you')
-    response.body.should == 'dummy_rack_app body'
-    response.status.should == 200
+  describe "using a custom app" do
+    before(:each) do
+      @stack = Rack::Builder.new do
+        use Dragonfly::Middleware, :images
+        run dummy_rack_app
+      end
+    end
+
+    it "should use the specified dragonfly app" do
+      Dragonfly.default_app.should_not_receive(:call)
+      Dragonfly[:images].should_receive(:call).and_return([
+        200, {"Content-Type" => 'text/plain'}, ['booboo']
+      ])
+      response = make_request(@stack, '/media/hello.png?howare=you')
+      response.body.should == 'booboo'
+    end
   end
 
-  it "should return a 404 if the app returns a 404" do
-    Dragonfly[:images].should_receive(:call).and_return(
-      [404, {"Content-Type" => 'text/plain'}, ['Not found']]
-    )
-    response = make_request(@stack, '/media/hello.png?howare=you')
-    response.status.should == 404
-  end
-
-  it "should return as per the dragonfly app if the app returns a 200" do
-    Dragonfly[:images].should_receive(:call).and_return(
-      [200, {"Content-Type" => 'text/plain'}, ['ABCD']]
-    )
-    response = make_request(@stack, '/media/hello.png?howare=you')
-    response.status.should == 200
-    response.body.should == 'ABCD'
-  end
-  
 end
