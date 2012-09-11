1d1e4324f417161268cbd40b1ab86095ea899bfd
Mark Evans <mark@new-bamboo.co.uk>
2012-09-11 16:43:28 +0100

    Added json_encode and json_decode

--- lib/dragonfly/serializer.rb
+++ lib/dragonfly/serializer.rb
@@ -1,32 +1,43 @@
 # encoding: utf-8
 require 'base64'
+require 'json'
 
 module Dragonfly
   module Serializer
-    
+
     # Exceptions
     class BadString < RuntimeError; end
-    
+
     extend self # So we can do Serializer.b64_encode, etc.
-    
+
     def b64_encode(string)
       Base64.encode64(string).tr("\n=",'').tr('/','~')
     end
-    
+
     def b64_decode(string)
       padding_length = string.length % 4
       Base64.decode64(string.tr('~','/') + '=' * padding_length)
     end
-    
+
     def marshal_encode(object)
       b64_encode(Marshal.dump(object))
     end
-    
+
     def marshal_decode(string)
       Marshal.load(b64_decode(string))
     rescue TypeError, ArgumentError => e
       raise BadString, "couldn't decode #{string} - got #{e}"
     end
-    
+
+    def json_encode(object)
+      b64_encode(JSON.generate(object))
+    end
+
+    def json_decode(string)
+      JSON.parse(b64_decode(string))
+    rescue JSON::ParserError => e
+      raise BadString, "couldn't decode #{string} - got #{e}"
+    end
+
   end
 end
