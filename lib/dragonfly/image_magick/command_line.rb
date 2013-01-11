4c127bb32e2b21654fa502a370fb923c8d0c5abc
Mark Evans <mark@new-bamboo.co.uk>
2013-01-11 11:44:32 +0000

    Command line just does the very basic convert and identify commands

--- lib/dragonfly/image_magick/command_line.rb
+++ lib/dragonfly/image_magick/command_line.rb
@@ -24,20 +24,8 @@ module Dragonfly
         tempfile
       end
 
-      def identify(temp_object)
-        # example of details string:
-        # myimage.png PNG 200x100 200x100+0+0 8-bit DirectClass 31.2kb
-        format, width, height, depth = raw_identify(temp_object).scan(/([A-Z0-9]+) (\d+)x(\d+) .+ (\d+)-bit/)[0]
-        {
-          :format => format.downcase.to_sym,
-          :width => width.to_i,
-          :height => height.to_i,
-          :depth => depth.to_i
-        }
-      end
-
-      def raw_identify(temp_object, args='')
-        shell.run identify_command, "#{args} #{shell.quote(temp_object.path)}"
+      def identify(temp_object, args="")
+        shell.run(identify_command, "#{args} #{shell.quote(temp_object.path)}").strip
       end
 
     end
