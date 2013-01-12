5c82c2f09aa40fb0b4028b447c9b8924bcc687bd
Michael Yang <mike@captain-refactor.co.za>
2013-01-13 00:38:08 +0200

    added relational_data_store

---
+++ lib/dragonfly/data_storage/relational_data_store.rb
@@ -0,0 +1,35 @@
+module Dragonfly
+  module DataStorage
+
+    class RelationalDataStore
+      include Serializer
+      include Configurable
+
+      configurable_attr :class_name, 'RelationalDragonFly'
+      configurable_attr :store_meta, true
+
+      def store(temp_object, opts={})
+        class_name.constantize.create! { |x|
+          x.data = temp_object.data
+          x.meta = Marshal.dump(temp_object.meta) if store_meta
+        }.id
+      rescue ActiveRecord::ActiveRecordError => e
+        raise UnableToStore, e.message
+      end
+
+      def retrieve(uid)
+        record = class_name.constantize.find(uid)
+        [record.data, store_meta ? Marshal.load(record.meta) : {}]
+      rescue ActiveRecord::ActiveRecordError => e
+        raise DataNotFound, "couldn't find file #{uid}"
+      end
+
+      def destroy(uid)
+        class_name.constantize.find(uid).destroy
+      rescue ActiveRecord::ActiveRecordError => e
+        raise DataNotFound, e.message
+      end
+    end
+
+  end
+end
