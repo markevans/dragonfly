3d1da80367ceb9a941dffa5aa1e7fbacdd7466ab
Michael Yang <mike@captain-refactor.co.za>
2013-01-13 09:10:29 +0200

    updated encoding / decoding of relational datastore to use base64 encoding / decoding

--- lib/dragonfly/data_storage/relational_data_store.rb
+++ lib/dragonfly/data_storage/relational_data_store.rb
@@ -11,7 +11,7 @@ module Dragonfly
       def store(temp_object, opts={})
         class_name.constantize.create! { |x|
           x.data = temp_object.data
-          x.meta = Marshal.dump(temp_object.meta) if store_meta
+          x.meta = marshal_encode(temp_object.meta) if store_meta
         }.id
       rescue ActiveRecord::ActiveRecordError => e
         raise UnableToStore, e.message
@@ -19,7 +19,7 @@ module Dragonfly
 
       def retrieve(uid)
         record = class_name.constantize.find(uid)
-        [record.data, store_meta ? Marshal.load(record.meta) : {}]
+        [record.data, store_meta ? marshal_decode(record.meta) : {}]
       rescue ActiveRecord::ActiveRecordError => e
         raise DataNotFound, "couldn't find file #{uid}"
       end
