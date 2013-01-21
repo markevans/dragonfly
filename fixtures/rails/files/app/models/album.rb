8c356dc523851b73af5a6f92cc95983670f5c0f7
Mark Evans <mark@new-bamboo.co.uk>
2013-01-21 16:51:12 +0000

    Rails doesn't allow mass-assignment by default now

--- fixtures/rails/files/app/models/album.rb
+++ fixtures/rails/files/app/models/album.rb
@@ -1,4 +1,5 @@
 class Album < ActiveRecord::Base
+  attr_accessible :name, :retained_cover_image, :cover_image
   validates_property :format, :of => :cover_image, :in => [:jpg, :png, :gif]
   validates_length_of :name, :in => 0..5
   image_accessor :cover_image
