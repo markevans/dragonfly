014673389f76c98d0367b352567672cd8f2ab631
Mark Evans <mark@new-bamboo.co.uk>
2013-01-22 11:09:52 +0000

    Copied Dragonfly::Utils from new_config branch

---
+++ lib/dragonfly/utils.rb
@@ -0,0 +1,24 @@
+require 'tempfile'
+
+module Dragonfly
+  module Utils
+
+    module_function
+
+    def new_tempfile(ext=nil, content=nil)
+      tempfile = ext ? Tempfile.new(['dragonfly', ".#{ext}"]) : Tempfile.new('dragonfly')
+      tempfile.binmode
+      tempfile.write(content) if content
+      tempfile.close
+      tempfile
+    end
+
+    def symbolize_keys(hash)
+      hash.inject({}) do |new_hash, (key, value)|
+        new_hash[key.to_sym] = value
+        new_hash
+      end
+    end
+
+  end
+end
