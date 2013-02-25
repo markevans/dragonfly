a5ddcca06de5c4edaf0864768b7d3bfe853a547e
Mark Evans <mark@new-bamboo.co.uk>
2013-02-25 17:12:53 +0000

    slightly unnecessary spec was giving annoying warning

--- spec/dragonfly/image_magick/generators/plain_spec.rb
+++ spec/dragonfly/image_magick/generators/plain_spec.rb
@@ -60,12 +60,6 @@ describe Dragonfly::ImageMagick::Generators::Plain do
     image.should have_width(1)
   end
 
-  it "should blow up with an invalid colour" do
-    lambda{
-      @generator.call(1,1,'rgb(doogie)')
-    }.should_not raise_error()
-  end
-
   describe "specifying the format" do
     before(:each) do
       @image, @meta = @generator.call(23, 12, 'white', :format => :gif)
