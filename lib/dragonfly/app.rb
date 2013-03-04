ced5b714e6d2435e2d584f48a1a6717aa7c801eb
Frederick Cheung <frederick.cheung@gmail.com>
2013-03-04 16:46:16 +0000

    Allow fallback to legacy urls to be turned off

--- lib/dragonfly/app.rb
+++ lib/dragonfly/app.rb
@@ -52,6 +52,7 @@ module Dragonfly
     configurable_attr :trust_file_extensions, true
     configurable_attr :content_disposition
     configurable_attr :content_filename, Dragonfly::Response::DEFAULT_FILENAME
+    configurable_attr :allow_legacy_urls, true
 
     attr_reader :analyser
     attr_reader :processor
