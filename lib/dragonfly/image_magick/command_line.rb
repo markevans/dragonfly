e8cf205cd1efd7b5d2aea0d71b3d2996cd5de9e5
Mark Evans <mark@new-bamboo.co.uk>
2013-05-30 09:23:56 +0100

    CommandLine only holds path for convert/identify now

--- lib/dragonfly/image_magick/command_line.rb
+++ lib/dragonfly/image_magick/command_line.rb
@@ -1,33 +1,13 @@
 module Dragonfly
   module ImageMagick
     class CommandLine
-
       def initialize
-        @shell = Shell.new
+        @convert_command = 'convert'
+        @identify_command = 'identify'
       end
 
-      attr_reader :shell
-
-      def convert_command
-        @convert_command ||= 'convert'
-      end
-      attr_writer :convert_command
-
-      def identify_command
-        @identify_command ||= 'identify'
-      end
-      attr_writer :identify_command
-
-      def convert(path=nil, args='', format=nil, tempfile=nil)
-        tempfile ||= Dragonfly::Utils.new_tempfile(format)
-        shell.run convert_command, %(#{shell.quote(path) if path} #{args} #{shell.quote(tempfile.path)})
-        tempfile
-      end
-
-      def identify(path, args="")
-        shell.run(identify_command, "#{args} #{shell.quote(path)}").strip
-      end
-
+      attr_accessor :convert_command
+      attr_accessor :identify_command
     end
   end
 end
