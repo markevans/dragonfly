ca02932538725a6cf5d94d28cae59d9ba9643cf2
Mark Evans <mark@new-bamboo.co.uk>
2013-08-14 17:03:07 +0100

    updated irbrc.rb

--- dev/irbrc.rb
+++ dev/irbrc.rb
@@ -3,15 +3,13 @@ require "bundler/setup"
 $:.unshift(File.expand_path('../../lib', __FILE__))
 require 'dragonfly'
 include Dragonfly::Serializer
-APP = Dragonfly[:images].configure_with(:imagemagick)
 
-# available_uids = `find #{APP.datastore.root_path} ! -type d`.split("\n").map do |file|
-#   file.sub("#{APP.datastore.root_path}/", '')
-# end
+APP = Dragonfly[:images].configure do
+  use :imagemagick
+  datastore :memory
+end
 
 puts "Loaded stuff from dragonfly irbrc"
-# puts "\nAvailable uids:\n"
-# puts available_uids
 puts "\nAvailable sample images:\n"
 puts Dir['samples/*']
 puts "\nAvailable constants:\n"
