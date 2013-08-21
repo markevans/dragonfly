a8f1b05292f5dd4e2670e1ff5137d1aa9f1e3332
Mark Evans <mark@new-bamboo.co.uk>
2013-08-21 12:48:57 +0100

    Added model to irbrc for local testing

--- dev/irbrc.rb
+++ dev/irbrc.rb
@@ -9,9 +9,18 @@ APP = Dragonfly.default_app.configure do
   datastore :memory
 end
 
+class Model
+  extend Dragonfly::Model
+  attr_accessor :image_uid, :image_name, :image_width
+  dragonfly_accessor :image
+end
+
 puts "Loaded stuff from dragonfly irbrc"
 puts "\nAvailable sample images:\n"
 puts Dir['samples/*']
 puts "\nAvailable constants:\n"
 puts "APP"
+puts "\nModel:\n"
+puts "dragonfly_accessor :image with image_name, image_width"
 puts
+
