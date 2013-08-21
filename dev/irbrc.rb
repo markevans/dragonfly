9cdd5a1d84e0b32e2d8571b1c18b7bcaedf1b712
Mark Evans <mark@new-bamboo.co.uk>
2013-08-21 18:08:57 +0100

    irbrc changes

--- dev/irbrc.rb
+++ dev/irbrc.rb
@@ -11,8 +11,9 @@ end
 
 class Model
   extend Dragonfly::Model
-  attr_accessor :image_uid, :image_name, :image_width
+  attr_accessor :image_uid, :image_name, :image_width, :small_image_uid
   dragonfly_accessor :image
+  dragonfly_accessor :small_image
 end
 
 puts "Loaded stuff from dragonfly irbrc"
