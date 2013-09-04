b1387456a6631f844cbcb0036a7ad27b695c09b2
Mark Evans <mark@new-bamboo.co.uk>
2013-09-04 12:55:15 +0100

    sanity check tests for changing convert/identify commands

--- spec/functional/shell_commands_spec.rb
+++ spec/functional/shell_commands_spec.rb
@@ -1,14 +1,12 @@
 require 'spec_helper'
 
-describe "a configured imagemagick app" do
-  
-  before(:each) do
-    @app = test_app.configure_with(:imagemagick)
-  end
-  
+describe "using the shell" do
+
+  let (:app) { test_app }
+
   describe "shell injection" do
-    
     it "should not allow it!" do
+      app.configure_with(:imagemagick)
       begin
         suppressing_stderr do
           @app.generate(:plain, 10, 10, 'white').convert("-resize 5x5 ; touch tmp/stuff").apply
@@ -17,7 +15,21 @@ describe "a configured imagemagick app" do
       end
       File.exist?('tmp/stuff').should be_false
     end
-    
   end
-  
+
+  describe "env variables with imagemagick" do
+    it "allows configuring the convert path" do
+      app.configure_with(:imagemagick, :convert_command => '/bin/convert')
+      app.shell.should_receive(:`).with(%r[/bin/convert])
+      app.create("").thumb('30x30').apply
+    end
+
+    it "allows configuring the identify path" do
+      app.configure_with(:imagemagick, :identify_command => '/bin/identify')
+      app.shell.should_receive(:`).with(%r[/bin/identify]).and_return("JPG 1 1")
+      app.create("").width
+    end
+  end
+
 end
+
