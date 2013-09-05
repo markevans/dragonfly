00c317cd03b9562b620a05ba248856b7cf52f43b
Mark Evans <mark@new-bamboo.co.uk>
2013-09-05 16:18:11 +0100

    script + rails application template for testing with rails

---
+++ dev/rails_template.rb
@@ -0,0 +1,38 @@
+gem 'dragonfly', :path => File.expand_path('../..', __FILE__)
+generate "dragonfly"
+generate "scaffold", "photo image_uid:string image_name:string"
+rake "db:migrate"
+route %(
+  get "text/:text" => Dragonfly.app.endpoint { |params, app|
+    app.generate(:text, params[:text])
+  }
+)
+route "root :to => 'photos#index'"
+run "rm -rf public/index.html"
+
+inject_into_file 'app/models/photo.rb', :after => "class Photo < ActiveRecord::Base\n" do
+  %(
+    attr_accessible :image rescue nil
+    dragonfly_accessor :image
+  )
+end
+
+gsub_file 'app/views/photos/_form.html.erb', /^.*:image_.*$/, ''
+
+inject_into_file 'app/views/photos/_form.html.erb', :before => %(<div class="actions">\n) do
+  %(
+    <div class="field">
+      <%= f.label :image %><br>
+      <%= f.file_field :image %>
+    </div>
+
+    <%= image_tag @photo.image.thumb('100x100').url if @photo.image_uid %>
+  )
+end
+
+gsub_file "app/controllers/photos_controller.rb", "permit(", "permit(:image, "
+
+append_file 'app/views/photos/show.html.erb', %(
+ <%= image_tag @photo.image.thumb('300x300').url if @photo.image_uid? %>
+)
+
