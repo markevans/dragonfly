a46f1a4a0e4fdc3453bf5769cf920d1e94f54194
Mark Evans <mark@new-bamboo.co.uk>
2013-09-17 17:04:43 +0100

    fixed shell_commands_spec - no idea why it was passing

--- spec/functional/shell_commands_spec.rb
+++ spec/functional/shell_commands_spec.rb
@@ -8,10 +8,8 @@ describe "using the shell" do
     it "should not allow it!" do
       app.configure_with(:imagemagick)
       begin
-        suppressing_stderr do
-          @app.generate(:plain, 10, 10, 'white').convert("-resize 5x5 ; touch tmp/stuff").apply
-        end
-      rescue Dragonfly::FunctionManager::UnableToHandle
+        app.generate(:plain, 10, 10, 'white').convert("-resize 5x5 ; touch tmp/stuff").apply
+      rescue Dragonfly::Shell::CommandFailed
       end
       File.exist?('tmp/stuff').should be_false
     end
@@ -20,13 +18,13 @@ describe "using the shell" do
   describe "env variables with imagemagick" do
     it "allows configuring the convert path" do
       app.configure_with(:imagemagick, :convert_command => '/bin/convert')
-      app.shell.should_receive(:`).with(%r[/bin/convert])
+      app.shell.should_receive(:run).with(%r[/bin/convert], hash_including)
       app.create("").thumb('30x30').apply
     end
 
     it "allows configuring the identify path" do
       app.configure_with(:imagemagick, :identify_command => '/bin/identify')
-      app.shell.should_receive(:`).with(%r[/bin/identify]).and_return("JPG 1 1")
+      app.shell.should_receive(:run).with(%r[/bin/identify], hash_including).and_return("JPG 1 1")
       app.create("").width
     end
   end
