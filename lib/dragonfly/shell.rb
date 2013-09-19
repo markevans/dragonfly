1e5e13739c0734d11106e1b980d4532b959987aa
Mark Evans <mark@new-bamboo.co.uk>
2013-09-19 16:13:32 +0100

    debug shell commands

--- lib/dragonfly/shell.rb
+++ lib/dragonfly/shell.rb
@@ -10,6 +10,7 @@ module Dragonfly
 
     def run(command, opts={})
       command = escape_args(command) unless opts[:escape] == false
+      Dragonfly.debug("shell command: #{command}")
       Open3.popen3 command do |stdin, stdout, stderr, wait_thread|
         status = wait_thread.value
         raise CommandFailed, "Command failed (#{command}) with exit status #{status.exitstatus} and stderr #{stderr.read}" unless status.success?
