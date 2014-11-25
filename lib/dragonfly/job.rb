e72914119a16b9cf5d1e3b7710319a6c8c638641
John Shearar <john.shearar@gmail.com>
2014-11-25 18:20:07 +0200

    better SHA algorithm

--- lib/dragonfly/job.rb
+++ lib/dragonfly/job.rb
@@ -256,7 +256,11 @@ module Dragonfly
     end
 
     def sha
-      Digest::SHA1.hexdigest("#{to_unique_s}#{app.secret}")[0...8]
+      unless app.secret
+        raise CannotGenerateSha, "A secret is required to sign and verify Dragonfly job requests. "\
+                                 "Use `secret '...'` or disable `protect_from_dos_attacks` in your config."
+      end
+      OpenSSL::HMAC.hexdigest('SHA256', app.secret, to_unique_s)[0,16]
     end
 
     def validate_sha!(sha)
