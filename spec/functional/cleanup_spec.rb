markevans/dragonfly

commit 56780f730f8ef3a6215c211cbb565f8a74568d55

moved #close into Content

lib/dragonfly/job.rb
@@ -216,7 +216,6 @@ module Dragonfly
       @app = app
       @steps = []
       @next_step_index = 0
-      @previous_temp_objects = []
       update(content, meta) if content
       @url_attrs = url_attrs ? url_attrs.dup : UrlAttributes.new
     end
@@ -231,10 +230,6 @@ module Dragonfly
     attr_reader :app, :steps
     attr_reader :temp_object
 
-    def temp_object=(temp_object)
-      previous_temp_objects.push(@temp_object) if @temp_object
-      @temp_object = temp_object
-    end
 
     # define fetch(), fetch!(), process(), etc.
     STEPS.each do |step_class|
@@ -393,11 +388,6 @@ module Dragonfly
       self.temp_object = TempObject.new(content, old_meta.merge(new_meta || {}))
     end
 
-    def close
-      previous_temp_objects.each{|temp_object| temp_object.close }
-      temp_object.close if temp_object
-    end
-
     def mime_type
       app.mime_type_for(ext)
     end
@@ -421,8 +411,6 @@ module Dragonfly
         attrs
       end
     end
-
-    attr_reader :previous_temp_objects
 
     def last_step_of_type(type)
       steps.select{|s| s.is_a?(type) }.last

lib/dragonfly/content.rb
@@ -10,6 +10,7 @@ module Dragonfly
     def initialize(app, obj=nil, meta=nil)
       @app = app
       @meta = {}
+      @previous_temp_objects = []
       update(obj, meta) if obj
     end
 
@@ -76,9 +77,18 @@ module Dragonfly
       update(tempfile)
     end
 
+    def close
+      previous_temp_objects.each{|temp_object| temp_object.close }
+      temp_object.close if temp_object
+    end
+
     private
 
-    attr_writer :temp_object
+    attr_reader :previous_temp_objects
+    def temp_object=(temp_object)
+      previous_temp_objects.push(@temp_object) if @temp_object
+      @temp_object = temp_object
+    end
 
   end
 end

spec/dragonfly/content_spec.rb
@@ -216,4 +216,26 @@ describe Dragonfly::Content do
     end
   end
 
+  describe "close" do
+    before(:each) do
+      @app = test_app
+      @app.add_processor(:upcase){|c| c.update("HELLO") }
+      @content = Dragonfly::Content.new(@app, "hello")
+      @temp_object1 = @content.temp_object
+      @content.process!(:upcase)
+      @temp_object2 = @content.temp_object
+      @temp_object1.should_not == @temp_object2 # just checking
+    end
+
+    it "should clean up tempfiles for the last temp_object" do
+      @temp_object2.should_receive(:close)
+      @content.close
+    end
+
+    it "should clean up tempfiles for previous temp_objects" do
+      @temp_object1.should_receive(:close)
+      @content.close
+    end
+  end
+
 end

spec/dragonfly/job_spec.rb
@@ -909,29 +909,4 @@ describe Dragonfly::Job do
       job.apply.name.should == 'egg.mumma'
     end
   end
-
-  describe "close" do
-    before(:each) do
-      @app = test_app
-      @app.add_generator(:toast){ "toast" }
-      @app.add_processor(:upcase){|t| t.data.upcase }
-      @job = @app.generate(:toast)
-      @path1 = @job.tempfile.path
-      @job.process!(:upcase)
-      @path2 = @job.tempfile.path
-    end
-
-    it "should clean up tempfiles for the last temp_object" do
-      File.exist?(@path2).should be_true
-      @job.close
-      File.exist?(@path2).should be_false
-    end
-
-    it "should clean up tempfiles for previous temp_objects" do
-      File.exist?(@path1).should be_true
-      @job.close
-      File.exist?(@path1).should be_false
-    end
-  end
-
 end

commit aa7eb74bed450131b1903dbdb9ed258a0929c0d1

unlink tempfiles on each request (was previously working but broken by 56780f73) - fixes https://github.com/markevans/dragonfly/issues/378

lib/dragonfly/job.rb
@@ -22,7 +22,8 @@ module Dragonfly
                    :data, :file, :tempfile, :path, :to_file, :size, :each,
                    :meta, :meta=, :add_meta, :name, :name=, :basename, :basename=, :ext, :ext=, :mime_type,
                    :analyse, :shell_eval, :store,
-                   :b64_data
+                   :b64_data,
+                   :close
 
     require 'dragonfly/job/fetch'
     require 'dragonfly/job/fetch_file'

spec/functional/cleanup_spec.rb
@@ -1,0 +1,22 @@
+require 'spec_helper'
+
+describe "cleaning up tempfiles" do
+
+  let (:app) {
+    test_app.configure{
+      processor :copy do |content|
+        content.shell_update do |old_path, new_path|
+          "cp #{old_path} #{new_path}"
+        end
+      end
+    }
+  }
+
+  it "unlinks tempfiles on each request" do
+    expect {
+      uid = app.store("blug")
+      url = app.fetch(uid).copy.url
+      request(app, url)
+    }.not_to increase_num_tempfiles
+  end
+end

spec/support/simple_matchers.rb
@@ -47,3 +47,14 @@ RSpec::Matchers.define :match_steps do |steps|
     given.map{|step| step.class } == steps
   end
 end
+
+RSpec::Matchers.define :increase_num_tempfiles do
+  match do |block|
+    num_tempfiles_before = Dir.entries(Dir.tmpdir).size
+    block.call
+    num_tempfiles_after = Dir.entries(Dir.tmpdir).size
+    increased = num_tempfiles_after > num_tempfiles_before
+    puts "Num tempfiles increased: #{num_tempfiles_before} -> #{num_tempfiles_after}" if increased
+    increased
+  end
+end