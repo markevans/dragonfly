82c52e2104e68b1d36a258810fbdaec9ce0052c5
Brendon Muir <brendon@spike.net.nz>
2018-09-20 17:11:55 +1200

    Add array_including
    Modified to allow regex string comparison.

--- spec/support/argument_matchers.rb
+++ spec/support/argument_matchers.rb
@@ -2,6 +2,27 @@ def string_matching(regexp)
   Spec::Mocks::ArgumentMatchers::RegexpMatcher.new(regexp)
 end
 
+class ArrayIncludingMatcher
+  def initialize(expected)
+    @expected = expected
+  end
+
+  def ==(actual)
+    @expected.all? {|v| actual.grep(v).size > 0}
+  rescue NoMethodError
+    false
+  end
+
+  def description
+    "array_including(#{@expected.inspect.sub(/^\{/,"").sub(/\}$/,"")})"
+  end
+end
+
+def array_including(*args)
+	actually_an_array = Array === args.first && args.count == 1 ? args.first : args
+  ArrayIncludingMatcher.new(actually_an_array)
+end
+
 class ContentArgumentMatcher
   def initialize(data)
     @data = data
