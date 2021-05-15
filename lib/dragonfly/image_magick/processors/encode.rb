bf14fb145f64c84e0e92f72fd7d1fe4b629a39b0
Mark Evans <markevans3000@gmail.com>
2021-05-15 15:59:52 +0100

    param validators for encode processor

--- lib/dragonfly/image_magick/processors/encode.rb
+++ lib/dragonfly/image_magick/processors/encode.rb
@@ -4,11 +4,23 @@ module Dragonfly
   module ImageMagick
     module Processors
       class Encode
+        include ParamValidators
+
+        WHITELISTED_ARGS = %w(quality)
+
+        IS_IN_WHITELISTED_ARGS = ->(args_string) {
+          args_string.scan(/-\w+/).all? { |arg|
+            WHITELISTED_ARGS.include?(arg.sub("-", ""))
+          }
+        }
+
         def update_url(attrs, format, args = "")
           attrs.ext = format.to_s
         end
 
         def call(content, format, args = "")
+          validate!(format, &is_word)
+          validate!(args, &IS_IN_WHITELISTED_ARGS)
           Commands.convert(content, args, "format" => format)
         end
       end
